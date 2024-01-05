734ef6c9dd177485e54c57b14065b49c2cc14552
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-01-05 13:49:14 +0100

    Minimalistic elasticsearch + kibana + clickhouse setup (#3)
    Co-authored-by: Krzysztof Kiewicz <krzysztof@quesma.com>

---
+++ log-generator/logger.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+import "math/rand"
+
+const url = "http://mitmproxy:8080/example/_doc"
+
+func main() {
+	for {
+		time.Sleep(time.Duration(1000+rand.Intn(5000)) * time.Millisecond)
+
+		body, err := json.Marshal(map[string]string{
+			"timestamp": time.Now().Format("2006-01-02T15:04:05.999Z"),
+			"message":   "Something happened!",
+			"severity":  "info",
+			"source":    "oracle",
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		resp.Body.Close()
+	}
+}
