5f9a8f31cefbb6f6ef3ce778b5b0f3a10cfd2bcd
Grzegorz Piwowarek <grzegorz@quesma.com>
2024-01-10 19:02:21 +0100

    GitHub Actions Smoke Test Pipeline (#21)
    Ensures that:
    - Kibana is alive
    - Elasticsearch is alive (and receiving logs)
    - Clickhouse is alive (and receiving logs)
    
    ---------
    
    Co-authored-by: Jacek Migdal <jacek@quesma.com>

---
+++ smoke-test/go.mod
@@ -0,0 +1,7 @@
+module mitmproxy/quesma
+
+go 1.21.5
+
+require github.com/mailru/go-clickhouse v1.8.0
+
+require github.com/google/uuid v1.2.0 // indirect
---
+++ smoke-test/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	_ "github.com/mailru/go-clickhouse"
+)
+
+const (
+	clickhouseUrl        = "http://localhost:8123"
+	kibanaHealthCheckUrl = "http://localhost:5601/api/status"
+	elasticIndexCountUrl = "http://localhost:9201/logs-generic-default/_count"
+)
+
+func main() {
+	waitForKibana()
+	waitForLogsInClickhouse()
+	waitForLogsInElasticsearch()
+}
+
+const waitInterval = 5 * time.Second
+const timeoutAfter = time.Minute
+
+func waitFor(serviceName string, waitForFunc func() bool) bool {
+	startTime := time.Now()
+
+	for time.Since(startTime) < timeoutAfter {
+		if waitForFunc() {
+			return true
+		}
+
+		fmt.Printf("elapsed %v, trying %s again in 5s...\n", time.Since(startTime), serviceName)
+		time.Sleep(waitInterval)
+	}
+
+	return false
+}
+
+func waitForLogsInClickhouse() {
+	res := waitFor("clickhouse", func() bool {
+		logCount := -1
+		connection, err := sql.Open("clickhouse", clickhouseUrl)
+		if err != nil {
+			panic(err)
+		}
+		defer connection.Close()
+
+		row := connection.QueryRow("SELECT COUNT(*) FROM `/logs-generic-default/_doc`")
+		_ = row.Scan(&logCount)
+
+		return logCount > 0
+	})
+
+	if !res {
+		panic("no logs in clickhouse")
+	}
+}
+
+func waitForKibana() {
+	res := waitFor("kibana", func() bool {
+		resp, err := http.Get(kibanaHealthCheckUrl)
+		if err == nil {
+			if resp.StatusCode == 200 {
+				return true
+			} else {
+				fmt.Printf("response: %+v\n", resp)
+			}
+		}
+		return false
+	})
+
+	if !res {
+		panic("kibana is not alive")
+	}
+}
+
+func waitForLogsInElasticsearch() {
+	res := waitFor("elasticsearch", func() bool {
+		resp, err := http.Get(elasticIndexCountUrl)
+		if err == nil {
+			defer resp.Body.Close()
+			if resp.StatusCode == 200 {
+				body, err := io.ReadAll(resp.Body)
+				if err == nil {
+					var response map[string]int
+					_ = json.Unmarshal(body, &response)
+					var foo = response["count"]
+					if foo > 0 {
+						return true
+					}
+				}
+			} else {
+				fmt.Printf("response: %+v\n", resp)
+			}
+		}
+		return false
+	})
+
+	if !res {
+		panic("elasticsearch is not alive or is not receiving logs")
+	}
+}
