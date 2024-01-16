afafb4d0e41833af2b21a270761bd652510b5b52
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-01-16 13:41:02 +0100

    Extend log message (#49)

--- quesma/quesma/io.go
+++ quesma/quesma/io.go
@@ -9,7 +9,7 @@ func copyAndSignal(signal chan struct{}, dst io.Writer, src io.Reader) {
 	defer quesmaRecover()
 	_, err := io.Copy(dst, src)
 	if err != nil {
-		log.Println(err)
+		log.Println("Copy error :" + err.Error())
 	}
 	signal <- struct{}{}
 }
