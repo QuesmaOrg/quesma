87f6e709fbb85e06435127e6032ddd02e2ebe64d
Grzegorz Piwowarek <grzegorz@quesma.com>
2024-01-19 20:11:39 +0100

    Predefine schema for '/device_logs/_doc (#85)
    ----
    Depends on:
    - https://github.com/QuesmaOrg/poc-elk-mitmproxy/pull/87

--- quesma/clickhouse/schemas.go
+++ quesma/clickhouse/schemas.go
@@ -11,26 +11,30 @@ var PredefinedTableSchemas = TableMap{
 		Database: "",
 		Cluster:  "",
 		Cols: map[string]*Column{
-			"event_name": {
-				Name: "event_name",
-				Type: BaseType{
-					Name:   "String",
-					goType: NewBaseType("String").goType,
-				},
-				Modifiers: "NOT NULL CODEC(ZSTD(1))",
-			},
+			"event_name":          lowCardinalityString("event_name"),
+			"event_section":       lowCardinalityString("event_section"),
+			"dedup_id":            genericString("dedup_id"),
+			"user_id":             genericString("user_id"),
+			"client_id":           genericString("client_id"),
+			"client_ip":           genericString("client_ip"),
+			"ts_day":              lowCardinalityString("ts_day"),
+			"et_day_hour":         genericString("et_day_hour"),
+			"ts_day_hour":         genericString("ts_day_hour"),
+			"ts_time_druid":       dateTime("ts_time_druid"), // TODO TZ
+			"epoch_time_original": int64CH("epoch_time_original"),
+			"epoch_time":          dateTime("epoch_time"), // TODO TZ
 		},
 		indexes: []IndexStatement{
 			getIndexStatement("event_name"),
 		},
 		Config: &ChTableConfig{
-			hasTimestamp:         true,
+			hasTimestamp:         false,
 			timestampDefaultsNow: false,
 			engine:               "MergeTree",
-			orderBy:              "(timestamp)",
-			partitionBy:          "",
+			orderBy:              "(epoch_time_original)",
+			partitionBy:          "toDate(epoch_time_original / 1000000000)",
 			primaryKey:           "",
-			ttl:                  "",
+			ttl:                  "toDateTime(epoch_time_original / 1000000000) + toIntervalSecond(1296000)",
 			settings:             "index_granularity = 8192, ttl_only_drop_parts = 1",
 			hasOthers:            false,
 			attributes: []Attribute{
@@ -40,4 +44,47 @@ var PredefinedTableSchemas = TableMap{
 			preferCastingToOthers:                 true,
 		},
 	},
+}
+
+func genericString(name string) *Column {
+	return &Column{
+		Name: name,
+		Type: BaseType{
+			Name:   "String",
+			goType: NewBaseType("String").goType,
+		},
+		Modifiers: "CODEC(ZSTD(1))",
+	}
+}
+
+func lowCardinalityString(name string) *Column {
+	return &Column{
+		Name: name,
+		Type: BaseType{
+			Name:   "LowCardinality(String)",
+			goType: NewBaseType("LowCardinality(String)").goType,
+		},
+	}
+}
+
+func dateTime(name string) *Column {
+	return &Column{
+		Name: name,
+		Type: BaseType{
+			Name:   "DateTime64",
+			goType: nil,
+		},
+		Modifiers: "CODEC(DoubleDelta, LZ4)",
+	}
+}
+
+func int64CH(name string) *Column {
+	return &Column{
+		Name: name,
+		Type: BaseType{
+			Name:   "Int64",
+			goType: NewBaseType("Int64").goType,
+		},
+		Modifiers: "CODEC(DoubleDelta, LZ4)",
+	}
 }
