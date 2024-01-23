69f79350857d5a7f491c668cf72c456e49d6c124
Grzegorz Piwowarek <grzegorz@quesma.com>
2024-01-23 18:57:57 +0100

    Expand predefined device schema (#131)

--- quesma/clickhouse/schemas.go
+++ quesma/clickhouse/schemas.go
@@ -1,5 +1,7 @@
 package clickhouse
 
+import "reflect"
+
 var NewRuntimeSchemas = make(TableMap)
 
 // A solution for now to remember tables. Didn't want to bother with config files at POC stage.
@@ -11,20 +13,43 @@ var PredefinedTableSchemas = TableMap{
 		Database: "",
 		Cluster:  "",
 		Cols: map[string]*Column{
-			"event_name":                      lowCardinalityString("event_name"),
-			"event_section":                   lowCardinalityString("event_section"),
-			"dedup_id":                        genericString("dedup_id"),
-			"user_id":                         genericString("user_id"),
-			"client_id":                       genericString("client_id"),
-			"client_ip":                       genericString("client_ip"),
-			"timestamps::topology_entry_time": genericString("timestamps::topology_entry_time"),
-			"ts_day":                          lowCardinalityString("ts_day"),
-			"et_day":                          genericString("et_day"),
-			"et_day_hour":                     genericString("et_day_hour"),
-			"ts_day_hour":                     genericString("ts_day_hour"),
-			"ts_time_druid":                   dateTime("ts_time_druid"), // TODO TZ
-			"epoch_time_original":             int64CH("epoch_time_original"),
-			"epoch_time":                      dateTime("epoch_time"), // TODO TZ
+			"event_name":                              lowCardinalityString("event_name"),
+			"event_section":                           lowCardinalityString("event_section"),
+			"dedup_id":                                genericString("dedup_id"),
+			"user_id":                                 genericString("user_id"),
+			"client_id":                               genericString("client_id"),
+			"client_ip":                               genericString("client_ip"),
+			"timestamps::topology_entry_time":         genericString("timestamps::topology_entry_time"),
+			"ts_day":                                  lowCardinalityString("ts_day"),
+			"et_day":                                  genericString("et_day"),
+			"et_day_hour":                             genericString("et_day_hour"),
+			"ts_day_hour":                             genericString("ts_day_hour"),
+			"ts_time_druid":                           dateTime("ts_time_druid"), // TODO TZ
+			"epoch_time_original":                     int64CH("epoch_time_original"),
+			"epoch_time":                              dateTime("epoch_time"), // TODO TZ
+			"properties::isreg":                       boolean("properties::isreg"),
+			"properties::enriched_client_ip":          genericString("properties::enriched_client_ip"),
+			"properties::enriched_app_id":             genericString("properties::enriched_app_id"),
+			"properties::enriched_user_id":            genericString("properties::enriched_user_id"),
+			"properties::app_id":                      genericString("properties::app_id"),
+			"properties::server_loc":                  genericString("properties::server_loc"),
+			"properties::pv_event":                    lowCardinalityString("properties::pv_event"),
+			"properties::ab_NewsStickyType":           lowCardinalityString("properties::ab_NewsStickyType"),
+			"properties::signed_state":                lowCardinalityString("properties::signed_state"),
+			"properties::country_detection_mechanism": lowCardinalityString("properties::country_detection_mechanism"),
+			"properties::enriched_user_language_primary": lowCardinalityString("properties::enriched_user_language_primary"),
+			"properties::selected_country":               lowCardinalityString("properties::selected_country"),
+			"properties::user_os_name":                   lowCardinalityString("properties::user_os_name"),
+			"properties::user_os_ver":                    lowCardinalityString("properties::user_os_ver"),
+			"properties::referrer_action":                lowCardinalityString("properties::referrer_action"),
+			"properties::user_type":                      lowCardinalityString("properties::user_type"),
+			"properties::user_language_primary":          lowCardinalityString("properties::user_language_primary"),
+			"properties::user_handset_model":             lowCardinalityString("properties::user_handset_model"),
+			"properties::user_handset_maker":             lowCardinalityString("properties::user_handset_maker"),
+			"properties::user_feed_type":                 lowCardinalityString("properties::user_feed_type"),
+			"properties::user_app_ver":                   lowCardinalityString("properties::user_app_ver"),
+			"properties::tabtype":                        lowCardinalityString("properties::tabtype"),
+			"properties::network_service_provider":       lowCardinalityString("properties::network_service_provider"),
 		},
 		indexes: []IndexStatement{
 			getIndexStatement("event_name"),
@@ -56,6 +81,14 @@ func genericString(name string) *Column {
 			goType: NewBaseType("String").goType,
 		},
 		Modifiers: "CODEC(ZSTD(1))",
+	}
+}
+
+func boolean(name string) *Column {
+	return &Column{
+		Name:      name,
+		Type:      BaseType{Name: "Bool", goType: reflect.TypeOf(true)},
+		Modifiers: "",
 	}
 }
 
