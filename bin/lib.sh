c9f47d02a7ce77d559469f076ec3265120c96fef
Jacek Migdal <jacek@quesma.com>
2024-03-06 15:44:19 +0100

    Usability for dev script (#467)
    I want also to start different env faster by typing:
    ```
    bin/up.sh hydrolix
    ```

--- bin/lib.sh
+++ bin/lib.sh
@@ -2,11 +2,16 @@
 
 DEFAULT_COMPOSE_FILE="docker/local-dev.yml"
 : "${QUESMA_COMPOSE_FILE:=$DEFAULT_COMPOSE_FILE}"
-if [ "$QUESMA_COMPOSE_FILE" != "$DEFAULT_COMPOSE_FILE" ]; then
-  echo "QUESMA_COMPOSE_FILE overriden to $QUESMA_COMPOSE_FILE"
+if [ ! -z "$1" ]; then
+  QUESMA_COMPOSE_FILE="docker/$1.yml"
+  echo "QUESMA_COMPOSE_FILE set to '$QUESMA_COMPOSE_FILE' by argument '$1'"
+else
+  if [ "$QUESMA_COMPOSE_FILE" != "$DEFAULT_COMPOSE_FILE" ]; then
+    echo "QUESMA_COMPOSE_FILE overriden by env to $QUESMA_COMPOSE_FILE"
+  fi
 fi
 
 if [ ! -f "$QUESMA_COMPOSE_FILE" ]; then
-  echo "File $QUESMA_COMPOSE_FILE does not exist."
+  echo "File '$QUESMA_COMPOSE_FILE' does not exist."
   exit 1
 fi
