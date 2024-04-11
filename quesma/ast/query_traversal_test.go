3984007e04e9fe6db9d4bd3708c35cbf7092af5d
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-04-11 17:28:35 +0200

    Adding QueryBasicVisitor, cleaning test (#717)

--- quesma/ast/query_traversal_test.go
+++ quesma/ast/query_traversal_test.go
@@ -29,49 +29,14 @@ const queryContent = `{
 type QueryCountingVisitor struct {
 	boolQueriesCounter int
 	termQueriesCounter int
+	QueryVisitor
 }
 
-func (QueryCountingVisitor) PreVisitQuery(q *types.Query, path []string)  {}
-func (QueryCountingVisitor) PostVisitQuery(q *types.Query, path []string) {}
 func (v *QueryCountingVisitor) PreVisitBool(b *types.BoolQuery, path []string) {
 	v.boolQueriesCounter++
 }
-func (QueryCountingVisitor) PostVisitBool(b *types.BoolQuery, path []string)              {}
-func (QueryCountingVisitor) PreVisitBoostingQuery(b *types.BoostingQuery, path []string)  {}
-func (QueryCountingVisitor) PostVisitBoostingQuery(b *types.BoostingQuery, path []string) {}
-func (QueryCountingVisitor) PreVisitTypeQuery(t *types.TypeQuery, path []string)          {}
-func (QueryCountingVisitor) PostVisitTypeQuery(t *types.TypeQuery, path []string)         {}
-func (QueryCountingVisitor) PreVisitCommonTermsQuery(ctq *types.CommonTermsQuery, field string, path []string) {
-}
-func (QueryCountingVisitor) PostVisitCommonTermsQuery(ctq *types.CommonTermsQuery, field string, path []string) {
-}
-func (QueryCountingVisitor) PreVisitCombinedFieldsQuery(ctq *types.CombinedFieldsQuery, path []string) {
-}
-func (QueryCountingVisitor) PostVisitCombinedFieldsQuery(ctq *types.CombinedFieldsQuery, path []string) {
-}
 func (v *QueryCountingVisitor) PreVisitTerm(t *types.TermQuery, field string, path []string) {
 	v.termQueriesCounter++
-}
-func (QueryCountingVisitor) PostVisitTerm(t *types.TermQuery, field string, path []string) {}
-func (QueryCountingVisitor) PreVisitConstantScoreQuery(csq *types.ConstantScoreQuery, path []string) {
-}
-func (QueryCountingVisitor) PostVisitConstantScoreQuery(csq *types.ConstantScoreQuery, path []string) {
-}
-func (QueryCountingVisitor) PreVisitDisMaxQuery(dmq *types.DisMaxQuery, path []string)  {}
-func (QueryCountingVisitor) PostVisitDisMaxQuery(dmq *types.DisMaxQuery, path []string) {}
-func (QueryCountingVisitor) PreVisitDistanceFeatureQuery(dfq types.DistanceFeatureQuery, path []string) {
-}
-func (QueryCountingVisitor) PostVisitDistanceFeatureQuery(dfq types.DistanceFeatureQuery, path []string) {
-}
-func (QueryCountingVisitor) PreVisitExistsQuery(eq *types.ExistsQuery, path []string)              {}
-func (QueryCountingVisitor) PostVisitExistsQuery(eq *types.ExistsQuery, path []string)             {}
-func (QueryCountingVisitor) PreVisitMatchQuery(mq *types.MatchQuery, field string, path []string)  {}
-func (QueryCountingVisitor) PostVisitMatchQuery(mq *types.MatchQuery, field string, path []string) {}
-func (QueryCountingVisitor) PreVisitMatchAllQuery(mq *types.MatchAllQuery, path []string)          {}
-func (QueryCountingVisitor) PostVisitMatchAllQuery(mq *types.MatchAllQuery, path []string)         {}
-func (QueryCountingVisitor) PreVisitMatchBoolPrefixQuery(mq *types.MatchBoolPrefixQuery, field string, path []string) {
-}
-func (QueryCountingVisitor) PostVisitMatchBoolPrefixQuery(mq *types.MatchBoolPrefixQuery, field string, path []string) {
 }
 
 func Test_queryTraversal(t *testing.T) {
@@ -80,7 +45,7 @@ func Test_queryTraversal(t *testing.T) {
 	err := json.Unmarshal(body, query)
 	require.NoError(t, err)
 	path := []string{}
-	visitor := &QueryCountingVisitor{}
+	visitor := &QueryCountingVisitor{QueryVisitor: QueryBasicVisitor{}}
 	queryTraverser := QueryTraverser{Debug: true, PathMatched: false}
 	queryTraverser.TraverseQuery(query, visitor, path)
 	assert.Equal(t, 2, visitor.boolQueriesCounter)
