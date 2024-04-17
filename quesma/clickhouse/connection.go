25bff16ccd3a1cc71886ae263d7bf3f0cad10d2d
Rafał Strzaliński <rafal@quesma.com>
2024-04-17 20:15:32 +0200

    Increase Clickhouse connection pool (#760)
    The default pool size is 10. It's pretty low. Quesma returns errors on
    following stress test:
    ```
    brew install hey
    hey -c 10 -n 10000 -z 5m 'http://localhost:8080/logs-*/_count'
    ```
    
    This should be configured in the config file. This PR doesn't address
    it.

--- quesma/clickhouse/connection.go
+++ quesma/clickhouse/connection.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"mitmproxy/quesma/buildinfo"
 	"mitmproxy/quesma/quesma/config"
+	"time"
 )
 
 func InitDBConnectionPool(c config.QuesmaConfiguration) *sql.DB {
@@ -29,7 +30,25 @@ func InitDBConnectionPool(c config.QuesmaConfiguration) *sql.DB {
 		Name:    "quesma",
 		Version: buildinfo.Version,
 	}
+
+	// Setting limit here is not working. It causes runtime error.
+	// Set it after opening the connection.
+	//
+	//	options.MaxIdleConns = 50
+	//	options.MaxOpenConns = 50
+	//	options.ConnMaxLifetime = 0
+
 	options.ClientInfo.Products = append(options.ClientInfo.Products, info)
 
-	return clickhouse.OpenDB(&options)
+	db := clickhouse.OpenDB(&options)
+
+	// The default is pretty low. We need to increase it.
+
+	// FIXME this should set in the configuration
+	db.SetMaxIdleConns(10)  // default is 5
+	db.SetMaxOpenConns(100) // default is 10
+	// clean up connections after 5 minutes, before that they may be killed by the firewallś
+	db.SetConnMaxLifetime(time.Duration(5) * time.Minute) // default is 1h
+
+	return db
 }
