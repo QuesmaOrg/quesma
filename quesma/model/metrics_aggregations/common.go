2621c8e7aeeed5295d901f9ae78e21601ad7ca7f
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-05-07 19:47:45 +0200

    Cleanup

--- quesma/model/metrics_aggregations/common.go
+++ quesma/model/metrics_aggregations/common.go
@@ -2,7 +2,6 @@ package metrics_aggregations
 
 import (
 	"context"
-	"fmt"
 	"mitmproxy/quesma/clickhouse"
 	"mitmproxy/quesma/logger"
 	"mitmproxy/quesma/model"
@@ -19,11 +18,10 @@ func metricsTranslateSqlResponseToJson(ctx context.Context, rows []model.QueryRe
 	}}
 }
 
-// same as metricsTranslateSqlResponseToJson for all types except DateTimeType
-// with DateTimes, we need to return 2 values, instead of 1.
+// metricsTranslateSqlResponseToJsonWithFieldTypeCheck is the same as metricsTranslateSqlResponseToJson for all types except DateTimes.
+// With DateTimes, we need to return 2 values, instead of 1, that's the difference.
 func metricsTranslateSqlResponseToJsonWithFieldTypeCheck(
 	ctx context.Context, rows []model.QueryResultRow, level int, fieldType clickhouse.DateTimeType) []model.JsonMap {
-	fmt.Println(fieldType)
 	if fieldType == clickhouse.Invalid {
 		// if it's not a date, we do just a normal response
 		return metricsTranslateSqlResponseToJson(ctx, rows, level)
@@ -32,7 +30,7 @@ func metricsTranslateSqlResponseToJsonWithFieldTypeCheck(
 	var value, valueAsString any = nil, nil
 	if resultRowsAreFine(ctx, rows) {
 		valueAsAny := rows[0].Cols[len(rows[0].Cols)-1].Value
-		if valueAsTime, isString := valueAsAny.(time.Time); isString {
+		if valueAsTime, ok := valueAsAny.(time.Time); ok {
 			value = valueAsTime.UnixMilli()
 			valueAsString = valueAsTime.Format(time.RFC3339Nano)
 		} else {
