225c3d0aae717a6b650b8566303399dd77807932
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-05-08 15:23:07 +0200

    BuildHistogramQuery is unused (#62)

--- quesma/queryparser/query_translator.go
+++ quesma/queryparser/query_translator.go
@@ -11,7 +11,6 @@ import (
 	"mitmproxy/quesma/util"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const facetsSampleSize = "20000"
@@ -586,30 +585,6 @@ func (cw *ClickhouseQueryTranslator) BuildAutocompleteQuery(fieldName, whereClau
 		FromClause:      cw.Table.FullTableName(),
 		CanParse:        true,
 	}
-}
-
-func (cw *ClickhouseQueryTranslator) BuildHistogramQuery(timestampFieldName, whereClauseOriginal, fixedInterval string) (*model.Query, time.Duration) {
-	var defaultInterval = 30 * time.Second
-	histogramOneBar, err := kibana.ParseInterval(fixedInterval)
-	if err != nil {
-		logger.ErrorWithCtx(cw.Ctx).Msg(err.Error())
-		histogramOneBar = defaultInterval
-	}
-	groupByClause := clickhouse.TimestampGroupBy(timestampFieldName, cw.Table.GetDateTimeType(cw.Ctx, timestampFieldName), histogramOneBar)
-	// [WARNING] This is a little oversimplified, but it seems to be good enough for now (==satisfies Kibana's histogram)
-	//
-	// In Elasticsearch's `date_histogram` aggregation implementation, the timestamps for the intervals are generated independently of the document data.
-	// The aggregation divides the specified time range into intervals based on the interval unit (e.g., minute, hour, day) and generates timestamps for each interval,
-	// irrespective of the actual timestamps of the documents.
-	query := model.Query{
-		Fields:          []string{},
-		NonSchemaFields: []string{groupByClause, "count()"},
-		WhereClause:     whereClauseOriginal,
-		SuffixClauses:   []string{"GROUP BY " + groupByClause},
-		FromClause:      cw.Table.FullTableName(),
-		CanParse:        true,
-	}
-	return &query, histogramOneBar
 }
 
 //lint:ignore U1000 Not used yet
