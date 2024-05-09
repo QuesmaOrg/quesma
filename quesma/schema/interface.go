df0f2878fde4179b69d247fe67653413333e8fc2
Jacek Migdal <jacek@quesma.com>
2024-05-09 14:08:15 +0200

    Basic interface

---
+++ quesma/schema/interface.go
@@ -0,0 +1,57 @@
+package schema
+
+type (
+	SearchConnector interface {
+		ResolveIndex(indexName string) (SearchIndex, error)
+	}
+
+	SearchIndex interface {
+		ResolveField(fieldName string) (SearchField, error)
+		AvailableFields() []SearchField
+
+		NewQueryBuilder() SearchQueryBuilder
+
+		DebugName() string
+	}
+
+	SearchField interface {
+		// E.g. returned in field caps
+		Name() string
+		IsSearchable() bool
+		IsAggregatable() bool
+		IsMetadataField() bool
+
+		// GetNestedField returned nested fields if they are present
+		GetNestedField() []SearchField
+
+		// NewComputedField Expression can be count(?), sum(?), avg(?), etc.
+		NewComputedField(expression string) (SearchField, error)
+		// NewWhereClause We may break it down to more methods
+		NewWhereClause(operator string, value interface{}) (WhereClause, error)
+
+		DebugName() string
+	}
+
+	WhereClause interface {
+		NewOrClause(clauses []WhereClause) WhereClause
+		NewAndClause(clauses []WhereClause) WhereClause
+		NewNotClause(clause WhereClause) WhereClause
+
+		DebugName() string
+	}
+
+	SearchQueryBuilder interface {
+		AddSelectStar()
+		AddSelect(field SearchField)
+
+		AddWhere(clause WhereClause)
+		AddWhereFullTextSearch(query string)
+
+		AddGroupBy(field SearchField)
+		AddOrderBy(field SearchField, asc bool)
+
+		SetSampleLimit(limit int)
+		SetLimit(limit int)
+		SetDistinct(isDistinct bool)
+	}
+)
