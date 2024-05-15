65f859183e7f9e1a10627f3bac5da1e2f8e692af
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-05-15 11:18:15 +0200

    Facets fix (#117)

--- quesma/clickhouse/quesma_communicator.go
+++ quesma/clickhouse/quesma_communicator.go
@@ -47,7 +47,12 @@ func (lm *LogManager) ProcessQuery(ctx context.Context, table *Table, query *mod
 	}
 	colNames, err := table.extractColumns(query, false)
 	sort.Strings(colNames)
-	sort.Strings(columns)
+	if columns == nil {
+		columns = lm.GetAllColumns(table, query)
+		// We should sort only if columns are not provided
+		// Caller is responsible for providing columns in the right order
+		sort.Strings(columns)
+	}
 	rowToScan := make([]interface{}, len(colNames)+len(query.NonSchemaFields))
 	if err != nil {
 		return nil, err
