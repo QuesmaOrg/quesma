9a52a30b8c3d9d95e826f9fec730931a26dbc6a6
przemyslaw <przemyslaw.hejman@quesma.com>
2024-05-16 11:45:12 +0200

    Comment out test case

--- quesma/testdata/unsupported_requests.go
+++ quesma/testdata/unsupported_requests.go
@@ -2020,18 +2020,20 @@ var UnsupportedQueriesTests = []UnsupportedQueryTestCase{
 			}
 		}`,
 	},
-	{ // [95]
-		TestName:  "Term-level queries: IDs",
-		QueryType: "ids",
-		QueryRequestJson: `
-		{
-			"query": {
-				"ids" : {
-					"values" : ["1", "4", "100"]
-				}
-			}
-		}`,
-	},
+	//{ // [95]
+	//	The query is partially supported, doesn't blow up,
+	// 	but the response is not as expected due to the nature of the backend (ClickHouse).
+	//	TestName:  "Term-level queries: IDs",
+	//	QueryType: "ids",
+	//	QueryRequestJson: `
+	//	{
+	//		"query": {
+	//			"ids" : {
+	//				"values" : ["1", "4", "100"]
+	//			}
+	//		}
+	//	}`,
+	//},
 	{ // [96]
 		TestName:  "Term-level queries: Regexp",
 		QueryType: "regexp",
