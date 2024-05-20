b5853e3ec1a9572395412a3a1411a00699864aa1
Rafał Strzaliński <rafal@quesma.com>
2024-05-20 11:48:24 +0200

    Fix test

--- quesma/quesma/matchers_test.go
+++ quesma/quesma/matchers_test.go
@@ -1,6 +1,7 @@
 package quesma
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"mitmproxy/quesma/quesma/mux"
 	"testing"
@@ -153,17 +154,26 @@ func TestMatchAgainstKibanaAlerts(t *testing.T) {
 		body     string
 		expected bool
 	}{
-		{"kibana alerts", kibanaAlerts, false},
+		{"{kibana alerts", kibanaAlerts, false},
 		{"non kibana alerts", nonKibanaAlerts, true},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(tt *testing.T) {
 
-			req := &mux.Request{Body: tt.body}
+			var parsed mux.ParsedJSON
+			if test.body != "" {
+
+				err := json.Unmarshal([]byte(test.body), &parsed)
+				if err != nil {
+					tt.Fatal(err)
+				}
+			}
+
+			req := &mux.Request{Body: test.body, JSON: parsed}
 
 			actual := matchAgainstKibanaAlerts().Matches(req)
-			assert.Equal(t, tt.expected, actual)
+			assert.Equal(t, test.expected, actual)
 		})
 
 	}
