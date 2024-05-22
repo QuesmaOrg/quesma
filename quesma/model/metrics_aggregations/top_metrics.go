de2e769599ac9d463c185828932915252c974b97
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-05-22 12:46:29 +0200

    A few small fixes to top_metrics, seems working

--- quesma/model/metrics_aggregations/top_metrics.go
+++ quesma/model/metrics_aggregations/top_metrics.go
@@ -2,8 +2,10 @@ package metrics_aggregations
 
 import (
 	"context"
+	"github.com/k0kubun/pp"
 	"mitmproxy/quesma/logger"
 	"mitmproxy/quesma/model"
+	"strconv"
 	"strings"
 )
 
@@ -28,13 +30,20 @@ func (query TopMetrics) TranslateSqlResponseToJson(rows []model.QueryResultRow, 
 			level, len(rows[0].Cols), len(rows),
 		)
 	}
+	pp.Println(rows[0], level)
 	for _, row := range rows {
 		lastIndex := len(row.Cols) - 1 // per convention, we know that value we sorted by is in the last column
 		metrics := make(model.JsonMap)
 		valuesForMetrics := row.Cols[:lastIndex]
 		sortVal := row.Cols[lastIndex].Value
 		for _, col := range valuesForMetrics[level:] {
-			colName, _ := strings.CutPrefix(col.ColName, "windowed_")
+			var withoutQuotes string
+			if unquoted, err := strconv.Unquote(col.ColName); err == nil {
+				withoutQuotes = unquoted
+			} else {
+				withoutQuotes = col.ColName
+			}
+			colName, _ := strings.CutPrefix(withoutQuotes, `windowed_`)
 			metrics[colName] = col.ExtractValue(query.ctx) // CHANGE IT AFTER PART 2 MERGE!! ENTER REAL CONTEXT FROM THE query
 		}
 		elem := model.JsonMap{
