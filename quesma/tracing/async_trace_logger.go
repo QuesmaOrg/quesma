11402a5107a30d6de2eed70de55031dc5098a41b
Jacek Migdal <jacek@quesma.com>
2024-05-31 17:31:22 +0200

    WIP

--- quesma/tracing/async_trace_logger.go
+++ quesma/tracing/async_trace_logger.go
@@ -38,56 +38,55 @@ func FormatMessages(messages []string) string {
 }
 
 func (h *AsyncTraceLogger) Run(e *zerolog.Event, level zerolog.Level, message string) {
+	var asyncId string
+	var ok bool
 	ctx := e.GetCtx()
-	if asyncId, ok := ctx.Value(AsyncIdCtxKey).(string); ok {
-		_, ok := ctx.Value(DumpedCtxKey).(bool)
-		if ok {
-			return
-		}
-		if len(asyncId) == 0 {
-			return
-		}
+	if asyncId, ok = ctx.Value(AsyncIdCtxKey).(string); !ok || len(asyncId) == 0 {
+		return // this processor just deal with async queries
+	}
+	if _, ok = ctx.Value(DumpedCtxKey).(bool); ok {
+		return // if we dump, we don't want to filter
+	}
 
-		if _, ok := ctx.Value(TraceEndCtxKey).(bool); ok {
-			e.Discard()
-			h.AsyncQueryTrace.Delete(asyncId)
-		} else if level == zerolog.ErrorLevel || level == zerolog.FatalLevel || level == zerolog.PanicLevel {
-			defer h.AsyncQueryTrace.Delete(asyncId)
-			defer e.Discard()
-			var traceCtx TraceCtx
-			traceCtx.Path = ""
-			if bufferedTraceCtx, ok := h.AsyncQueryTrace.Load(asyncId); ok {
-				traceCtx.Messages = append(traceCtx.Messages, bufferedTraceCtx.Messages...)
-			}
-			traceCtx.Messages = append(traceCtx.Messages, message)
-			traceCtx.Updated = time.Now()
-			ctx = context.WithValue(ctx, DumpedCtxKey, true)
-			e = e.Ctx(ctx)
-			var formattedLines strings.Builder
-			formattedLines.WriteString(FormatMessages(traceCtx.Messages))
-			// Below e.Msgf call is recursive one which means that after it we are inside outer Run method
-			// to avoid recursion we need to call Discard method
-			// We could call Str() here and avoid some
-			// recursion checks unnecessary
-			// however it would prevent us from custom formatting
-			e.Msgf("Async query error trace: %s", formattedLines.String())
-		} else {
-			// Buffer all non-error messages and discard them
-			if h.AsyncQueryTrace != nil {
-				if traceCtx, ok := h.AsyncQueryTrace.Load(asyncId); ok {
-					traceCtx.Messages = append(traceCtx.Messages, message)
-					traceCtx.Updated = time.Now()
-					h.AsyncQueryTrace.Store(asyncId, traceCtx)
-				} else {
-					traceCtx := TraceCtx{
-						Messages: []string{message},
-						Path:     "",
-						Added:    time.Now(),
-					}
-					h.AsyncQueryTrace.Store(asyncId, traceCtx)
+	if _, ok := ctx.Value(TraceEndCtxKey).(bool); ok {
+		e.Discard()
+		h.AsyncQueryTrace.Delete(asyncId)
+	} else if level == zerolog.ErrorLevel || level == zerolog.FatalLevel || level == zerolog.PanicLevel {
+		defer h.AsyncQueryTrace.Delete(asyncId)
+		defer e.Discard()
+		var traceCtx TraceCtx
+		traceCtx.Path = ""
+		if bufferedTraceCtx, ok := h.AsyncQueryTrace.Load(asyncId); ok {
+			traceCtx.Messages = append(traceCtx.Messages, bufferedTraceCtx.Messages...)
+		}
+		traceCtx.Messages = append(traceCtx.Messages, message)
+		traceCtx.Updated = time.Now()
+		ctx = context.WithValue(ctx, DumpedCtxKey, true)
+		e = e.Ctx(ctx)
+		var formattedLines strings.Builder
+		formattedLines.WriteString(FormatMessages(traceCtx.Messages))
+		// Below e.Msgf call is recursive one which means that after it we are inside outer Run method
+		// to avoid recursion we need to call Discard method
+		// We could call Str() here and avoid some
+		// recursion checks unnecessary
+		// however it would prevent us from custom formatting
+		e.Msgf("Async query error trace: %s", formattedLines.String())
+	} else {
+		// Buffer all non-error messages and discard them
+		if h.AsyncQueryTrace != nil {
+			if traceCtx, ok := h.AsyncQueryTrace.Load(asyncId); ok {
+				traceCtx.Messages = append(traceCtx.Messages, message)
+				traceCtx.Updated = time.Now()
+				h.AsyncQueryTrace.Store(asyncId, traceCtx)
+			} else {
+				traceCtx := TraceCtx{
+					Messages: []string{message},
+					Path:     "",
+					Added:    time.Now(),
 				}
-				e.Discard()
+				h.AsyncQueryTrace.Store(asyncId, traceCtx)
 			}
+			e.Discard()
 		}
 	}
 }
