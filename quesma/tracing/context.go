b1be01dd617878fae02ad1c88430d716cbae0a7e
Jacek Migdal <jacek@quesma.com>
2024-06-03 12:56:07 +0200

    Always pass request and async id

--- quesma/tracing/context.go
+++ quesma/tracing/context.go
@@ -1,6 +1,7 @@
 package tracing
 
 import (
+	"context"
 	"github.com/google/uuid"
 )
 
@@ -13,6 +14,19 @@ const (
 	AsyncIdCtxKey   ContextKey = "AsyncId"
 	TraceEndCtxKey  ContextKey = "TraceEnd"
 )
+
+// NewContextWithRequest creates a new context with the request id and async id from the existing context.
+// This is useful for async operations, where we want different cancel functions.
+func NewContextWithRequest(existingCtx context.Context) context.Context {
+	newContext := context.Background()
+	if requestId := existingCtx.Value(RequestIdCtxKey); requestId != nil {
+		newContext = context.WithValue(newContext, RequestIdCtxKey, requestId)
+	}
+	if asyncId := existingCtx.Value(AsyncIdCtxKey); asyncId != nil {
+		newContext = context.WithValue(newContext, AsyncIdCtxKey, asyncId)
+	}
+	return newContext
+}
 
 func GetRequestId() string {
 	return uuid.New().String()
