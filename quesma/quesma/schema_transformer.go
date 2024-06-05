2365571c1be2af83f31ff5ef4d0cf4818df4524d
Przemek Delewski <pdelewski@quesma.com>
2024-06-05 16:51:34 +0200

    Initial ip schema transformation

--- quesma/quesma/schema_transformer.go
+++ quesma/quesma/schema_transformer.go
@@ -1,10 +1,85 @@
 package quesma
 
-import "mitmproxy/quesma/model"
+import (
+	"mitmproxy/quesma/model"
+	"mitmproxy/quesma/queryparser/where_clause"
+	"strconv"
+)
+
+type WhereVisitor struct {
+	lhs string
+	rhs string
+}
+
+func (v *WhereVisitor) VisitLiteral(e *where_clause.Literal) interface{} {
+	return e.Name
+}
+
+func (v *WhereVisitor) VisitInfixOp(e *where_clause.InfixOp) interface{} {
+	if e.Left != nil {
+		lhs := e.Left.Accept(v)
+		v.lhs = lhs.(string)
+	}
+	if e.Right != nil {
+		rhs := e.Right.Accept(v)
+		v.rhs = rhs.(string)
+	}
+	return ""
+}
+
+func (v *WhereVisitor) VisitPrefixOp(*where_clause.PrefixOp) interface{} {
+	return ""
+}
+
+func (v *WhereVisitor) VisitFunction(*where_clause.Function) interface{} {
+	return ""
+}
+
+func (v *WhereVisitor) VisitColumnRef(e *where_clause.ColumnRef) interface{} {
+	return strconv.Quote(e.ColumnName)
+}
+
+func (v *WhereVisitor) VisitNestedProperty(*where_clause.NestedProperty) interface{} {
+	return ""
+}
+
+func (v *WhereVisitor) VisitArrayAccess(*where_clause.ArrayAccess) interface{} {
+	return ""
+}
 
 type SchemaCheckPass struct {
 }
 
 func (s *SchemaCheckPass) Transform(queries []model.Query) ([]model.Query, error) {
+	const isIPAddressInRangePrimitive = "isIPAddressInRange"
+	const CASTPrimitive = "CAST"
+	const StringLiteral = "'String'"
+
+	for k, query := range queries {
+		if query.WhereClause == nil {
+			continue
+		}
+		whereVisitor := &WhereVisitor{}
+		query.WhereClause.Accept(whereVisitor)
+		// TODO check this against the schema
+		if whereVisitor.lhs != "\"clientip\"" {
+			continue
+		}
+		transformedWhereClause := &where_clause.Function{
+			Name: where_clause.Literal{Name: isIPAddressInRangePrimitive},
+			Args: []where_clause.Statement{
+				&where_clause.Function{
+					Name: where_clause.Literal{Name: CASTPrimitive},
+					Args: []where_clause.Statement{
+						&where_clause.Literal{Name: whereVisitor.lhs},
+						&where_clause.Literal{Name: StringLiteral},
+					},
+				},
+				&where_clause.Literal{Name: whereVisitor.rhs},
+			},
+		}
+		query.WhereClause = transformedWhereClause
+		queries[k] = query
+	}
 	return queries, nil
 }
