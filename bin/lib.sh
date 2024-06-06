f3050532a3f90c88248a518d1276ad3a965d9cb4
Rafał Strzaliński <rafal@quesma.com>
2024-06-06 10:46:20 +0200

    Update bin/lib.sh
    Co-authored-by: Przemysław Hejman <przemyslaw.hejman@quesma.com>

--- bin/lib.sh
+++ bin/lib.sh
@@ -17,7 +17,7 @@ if [ ! -f "$QUESMA_COMPOSE_FILE" ]; then
 fi
 
 # Get the current git commit hash
-QUESMA_BUILD_DATE=$(git --no-pager log -1 --format="%ai")
+QUESMA_BUILD_DATE=$(git --no-pager log -1 --date=format:'%Y-%m-%d' --format="%ad")
 QUESMA_VERSION=$(git describe)
 QUESMA_BUILD_SHA=$(git rev-parse --short HEAD)
 
