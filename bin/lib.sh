01137bc248e1184d9b3a9b376dec9b76805c73af
Jacek Migdal <jacek@quesma.com>
2024-06-06 15:51:47 +0200

    Debugging

--- bin/lib.sh
+++ bin/lib.sh
@@ -3,7 +3,10 @@
 DEFAULT_COMPOSE_FILE="docker/local-dev.yml"
 : "${QUESMA_COMPOSE_FILE:=$DEFAULT_COMPOSE_FILE}"
 if [ ! -z "$1" ]; then
-  QUESMA_COMPOSE_FILE="docker/$1.yml"
+  QUESMA_COMPOSE_FILE="$1" # it can be file na,e
+  if [ ! -f "$QUESMA_COMPOSE_FILE" ]; then # orname
+    QUESMA_COMPOSE_FILE="docker/$1.yml"
+  fi
   echo "QUESMA_COMPOSE_FILE set to '$QUESMA_COMPOSE_FILE' by argument '$1'"
 else
   if [ "$QUESMA_COMPOSE_FILE" != "$DEFAULT_COMPOSE_FILE" ]; then
