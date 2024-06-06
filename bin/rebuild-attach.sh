78a98b67713eddc14a48515dd5ed9fc48427848e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-06-06 13:33:56 +0200

    Fix rebuild-attach.sh (#274)

--- bin/rebuild-attach.sh
+++ bin/rebuild-attach.sh
@@ -5,6 +5,6 @@ set -e
 cd "$(dirname "$0/")/.."
 source bin/lib.sh
 
-docker compose  -f "$QUESMA_COMPOSE_FILE" build "$QUESMA_COMPOSE_FILE" build --build-arg QUESMA_BUILD_DATE="$QUESMA_BUILD_DATE" --build-arg QUESMA_VERSION="$QUESMA_VERSION" --build-arg QUESMA_BUILD_SHA="$QUESMA_BUILD_SHA" quesma && \
+docker compose  -f "$QUESMA_COMPOSE_FILE" build --build-arg QUESMA_BUILD_DATE="$QUESMA_BUILD_DATE" --build-arg QUESMA_VERSION="$QUESMA_VERSION" --build-arg QUESMA_BUILD_SHA="$QUESMA_BUILD_SHA" quesma && \
   docker compose -f "$QUESMA_COMPOSE_FILE" stop quesma && \
   docker compose -f "$QUESMA_COMPOSE_FILE" up --no-deps quesma
