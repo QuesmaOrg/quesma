98c0e3189d712136f0fb2f0365ec7c3917bf8aa6
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-06-11 18:58:56 +0200

    Request saver works

--- quesma/quesma/field_caps_test.go
+++ quesma/quesma/field_caps_test.go
@@ -96,7 +96,7 @@ func TestFieldCaps(t *testing.T) {
 	err = json.Unmarshal(expectedResp, &expectedResp)
 	assert.NoError(t, err)
 
-	difference1, difference2, err := util.JsonDifference(
+	difference1, difference2, err := util.JsonDifferenceWeak(
 		string(resp),
 		string(expectedResp),
 	)
@@ -155,7 +155,7 @@ func TestFieldCapsWithStaticSchema(t *testing.T) {
 	err = json.Unmarshal(expectedResp, &expectedResp)
 	assert.NoError(t, err)
 
-	difference1, difference2, err := util.JsonDifference(
+	difference1, difference2, err := util.JsonDifferenceWeak(
 		string(resp),
 		string(expectedResp),
 	)
@@ -219,7 +219,7 @@ func TestFieldCapsMultipleIndexes(t *testing.T) {
 	err = json.Unmarshal(expectedResp, &expectedResp)
 	assert.NoError(t, err)
 
-	difference1, difference2, err := util.JsonDifference(
+	difference1, difference2, err := util.JsonDifferenceWeak(
 		string(resp),
 		string(expectedResp),
 	)
@@ -289,7 +289,7 @@ func TestFieldCapsMultipleIndexesConflictingEntries(t *testing.T) {
 	err = json.Unmarshal(expectedResp, &expectedResp)
 	assert.NoError(t, err)
 
-	difference1, difference2, err := util.JsonDifference(
+	difference1, difference2, err := util.JsonDifferenceWeak(
 		string(resp),
 		string(expectedResp),
 	)
