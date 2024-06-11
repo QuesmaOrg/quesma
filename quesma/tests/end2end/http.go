055fb36fd7f627be8589b2c9c2dbb944ea414d66
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-06-12 00:55:28 +0200

    MVP done

--- quesma/tests/end2end/http.go
+++ quesma/tests/end2end/http.go
@@ -15,12 +15,10 @@ func newHttpClient() HttpClient {
 	return HttpClient{&http.Client{}}
 }
 
-func (cli *HttpClient) sendPost(url string) (string, error) {
+func (cli *HttpClient) sendPost(url, body string) (string, error) {
 	fmt.Println("URL:>", url)
 
-	// var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(testRequests[0])))
-	req.Header.Set("X-Custom-Header", "myvalue")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := cli.client.Do(req)
@@ -32,20 +30,16 @@ func (cli *HttpClient) sendPost(url string) (string, error) {
 	fmt.Println("response Status:", resp.Status)
 	fmt.Println("response Headers:", resp.Header)
 
-	body, _ := io.ReadAll(resp.Body)
-	// fmt.Println("body:", string(body))
-	return string(body), nil
+	response, err := io.ReadAll(resp.Body)
+	return string(response), err
 }
 
-func (cli *HttpClient) sendSearchRequestToElastic(index, body string) (string, error) {
-	//const urlPrefix = "http://elasticsearch_direct:9200/"
-	const urlPrefix = "http://localhost:9202/"
-	const urlSuffix = "/_search"
-	return cli.sendPost(urlPrefix + index + urlSuffix)
+func (cli *HttpClient) sendRequestToElastic(urlSuffix, body string) (string, error) {
+	const urlPrefix = "http://localhost:9202"
+	return cli.sendPost(urlPrefix+urlSuffix, body)
 }
 
-func (cli *HttpClient) sendSearchRequestToQuesma(index, body string) (string, error) {
-	const urlPrefix = "http://localhost:8080/"
-	const urlSuffix = "/_search"
-	return cli.sendPost(urlPrefix + index + urlSuffix)
+func (cli *HttpClient) sendRequestToQuesma(urlSuffix, body string) (string, error) {
+	const urlPrefix = "http://localhost:8080"
+	return cli.sendPost(urlPrefix+urlSuffix, body)
 }
