98327c68a276c7d7de82af05d2c655007d56bb12
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-06-12 16:44:06 +0200

    Query syntax error, wrong column name (cannot be single quoted) (#319)

--- quesma/testdata/aggregation_requests.go
+++ quesma/testdata/aggregation_requests.go
@@ -1998,7 +1998,7 @@ var AggregationTests = []AggregationTestCase{
 				`maxOrNull("order_date") AS "windowed_order_date" FROM ` +
 				`(SELECT "order_date" , "order_date" , ROW_NUMBER() OVER ` +
 				`(PARTITION BY toInt64(toUnixTimestamp64Milli("order_date") / 43200000) ` +
-				`ORDER BY "order_date" ASC ) AS 'row_number' FROM ` + QuotedTableName + " " +
+				`ORDER BY "order_date" ASC ) AS row_number FROM ` + QuotedTableName + " " +
 				`WHERE (("order_date">=parseDateTime64BestEffort('2024-02-06T09:59:57.034Z') AND ` +
 				`"order_date"<=parseDateTime64BestEffort('2024-02-13T09:59:57.034Z')) AND "taxful_total_price" > '250')) ` +
 				`WHERE ((("order_date">=parseDateTime64BestEffort('2024-02-06T09:59:57.034Z') AND ` +
@@ -2009,7 +2009,7 @@ var AggregationTests = []AggregationTestCase{
 				`maxOrNull("order_date") AS "windowed_order_date" FROM ` +
 				`(SELECT "taxful_total_price" , "order_date" , ROW_NUMBER() OVER ` +
 				`(PARTITION BY toInt64(toUnixTimestamp64Milli("order_date") / 43200000) ` +
-				`ORDER BY "order_date" ASC ) AS 'row_number' FROM ` + QuotedTableName + " " +
+				`ORDER BY "order_date" ASC ) AS row_number FROM ` + QuotedTableName + " " +
 				`WHERE (("order_date">=parseDateTime64BestEffort('2024-02-06T09:59:57.034Z') AND ` +
 				`"order_date"<=parseDateTime64BestEffort('2024-02-13T09:59:57.034Z')) AND "taxful_total_price" > '250')) ` +
 				`WHERE ((("order_date">=parseDateTime64BestEffort('2024-02-06T09:59:57.034Z') AND ` +
@@ -5414,7 +5414,7 @@ var AggregationTests = []AggregationTestCase{
 				`FROM (SELECT "message" , "order_date" , ROW_NUMBER() OVER ` +
 				`(PARTITION BY toInt64(toUnixTimestamp64Milli("@timestamp") / 86400000) ` +
 				`ORDER BY "order_date" DESC ) ` +
-				`AS 'row_number' ` +
+				`AS row_number ` +
 				`FROM ` + QuotedTableName + ` ` +
 				`WHERE "message" IS NOT NULL) ` +
 				`WHERE ("message" IS NOT NULL ` +
