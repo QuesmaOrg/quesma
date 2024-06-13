711bb8a44387f78c7faa5520cbf755c5c0ec6799
Rafał Strzaliński <rafal@quesma.com>
2024-06-13 16:01:23 +0200

    comment

--- quesma/plugins/registry/plugin_registry.go
+++ quesma/plugins/registry/plugin_registry.go
@@ -8,11 +8,12 @@ import (
 // TODO plugins registry
 // TODO plugins finder
 
-// now
+// legacy, tests are passing with this
 var DefaultPlugin plugins.Plugin = &elastic_clickhouse_fields.LegacyClickhouseDoubleColonsPlugin{}
 
-// near future
-// var DefaultPlugin plugins.Plugin = &elastic_clickhouse_fields.ClickhouseDoubleColonsElasticDotsPlugin{}
+// we return fields in the format of elastic
+// it may require fixing the tests, but kibana works with this
+//var DefaultPlugin plugins.Plugin = &elastic_clickhouse_fields.ClickhouseDoubleColonsElasticDotsPlugin{}
 
 // ultimate future
 //var DefaultPlugin plugins.Plugin = &elastic_clickhouse_fields.ClickhouseSQLNativeLElasticDotsPlugin{}
