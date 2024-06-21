2cb2d6ba35e4b8071f928c1271370eaf0d8efdc1
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-06-21 08:18:28 +0200

    Fixing geo response (#370)

--- quesma/model/metrics_aggregations/top_hits.go
+++ quesma/model/metrics_aggregations/top_hits.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"mitmproxy/quesma/logger"
 	"mitmproxy/quesma/model"
+	"mitmproxy/quesma/schema"
+	"strconv"
+	"strings"
 )
 
 type TopHits struct {
@@ -20,17 +23,67 @@ func (query TopHits) IsBucketAggregation() bool {
 
 // TODO implement correct
 func (query TopHits) TranslateSqlResponseToJson(rows []model.QueryResultRow, level int) []model.JsonMap {
-	response := make([]model.JsonMap, len(rows))
-	if len(rows) > 0 && level >= len(rows[0].Cols) {
-		logger.WarnWithCtx(query.ctx).Msg("no columns returned for top_hits aggregation")
+	var topElems []any
+	if len(rows) > 0 && level >= len(rows[0].Cols)-1 {
+		// values are [level, len(row.Cols) - 1]
+		logger.WarnWithCtx(query.ctx).Msgf(
+			"no columns returned for top_hits aggregation, level: %d, len(rows[0].Cols): %d, len(rows): %d",
+			level, len(rows[0].Cols), len(rows),
+		)
 	}
-	for i, row := range rows {
-		response[i] = make(model.JsonMap, len(row.Cols)-level)
-		for _, col := range row.Cols[level:] {
-			response[i][col.ColName] = col.Value
+	for _, row := range rows {
+		if len(row.Cols) == 0 {
+			logger.WarnWithCtx(query.ctx).Msg("no columns returned for top_hits aggregation, skipping")
+			continue
 		}
+
+		valuesForHits := row.Cols
+		sourceMap := model.JsonMap{}
+
+		for _, col := range valuesForHits {
+			var withoutQuotes string
+			if unquoted, err := strconv.Unquote(col.ColName); err == nil {
+				withoutQuotes = unquoted
+			} else {
+				withoutQuotes = col.ColName
+			}
+			colName, _ := strings.CutPrefix(withoutQuotes, `windowed_`)
+
+			if col.ColType.Name == schema.TypePoint.Name {
+				hits := make(model.JsonMap)
+				// TODO suffixes (::lat, ::lon) hardcoded for now
+				// due to insufficient information in the schema
+				if strings.Contains(col.ColName, "::lon") {
+					hits["lon"] = col.ExtractValue(query.ctx)
+					colName = strings.TrimSuffix(col.ColName, "::lon")
+				}
+				if strings.Contains(col.ColName, "::lat") {
+					hits["lat"] = col.ExtractValue(query.ctx)
+					colName = strings.TrimSuffix(col.ColName, "::lat")
+				}
+				if _, ok := sourceMap[colName]; ok {
+					currentHits := sourceMap[colName].(model.JsonMap)
+					for k, v := range currentHits {
+						hits[k] = v
+					}
+					sourceMap[colName] = hits
+				} else {
+					sourceMap[colName] = hits
+				}
+
+			} else {
+				sourceMap[col.ColName] = col.ExtractValue(query.ctx)
+			}
+		}
+
+		elem := model.JsonMap{
+			"_source": sourceMap,
+		}
+		topElems = append(topElems, elem)
 	}
-	return response
+	return []model.JsonMap{{
+		"hits": topElems,
+	}}
 }
 
 func (query TopHits) String() string {
