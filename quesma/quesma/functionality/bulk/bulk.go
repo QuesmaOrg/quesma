54d95597a0cb5e0773e4795fa2e332b2ff375945
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-06-24 12:25:25 +0200

    Unify and extract withConfiguration()

--- quesma/quesma/functionality/bulk/bulk.go
+++ quesma/quesma/functionality/bulk/bulk.go
@@ -11,7 +11,6 @@ import (
 	"quesma/stats"
 	"quesma/stats/errorstats"
 	"quesma/telemetry"
-	"sync/atomic"
 )
 
 type (
@@ -83,7 +82,7 @@ func Write(ctx context.Context, defaultIndex *string, bulk types.NDJSON, lm *cli
 	for indexName, documents := range indicesWithDocumentsToInsert {
 		phoneHomeAgent.IngestCounters().Add(indexName, int64(len(documents)))
 
-		withConfiguration(ctx, cfg, indexName, make(types.JSON), func() error {
+		config.RunConfigured(ctx, cfg, indexName, make(types.JSON), func() error {
 			for _, document := range documents {
 				stats.GlobalStatistics.Process(cfg, indexName, document, clickhouse.NestedSeparator)
 			}
@@ -91,56 +90,4 @@ func Write(ctx context.Context, defaultIndex *string, bulk types.NDJSON, lm *cli
 		})
 	}
 	return results
-}
-
-var insertCounter = atomic.Int32{}
-
-func withConfiguration(ctx context.Context, cfg config.QuesmaConfiguration, indexName string, body types.JSON, action func() error) {
-	if len(cfg.IndexConfig) == 0 {
-		logger.InfoWithCtx(ctx).Msgf("%s  --> clickhouse, body(shortened): %s", indexName, body.ShortString())
-		err := action()
-		if err != nil {
-			logger.ErrorWithCtx(ctx).Msg("Can't write to index: " + err.Error())
-		}
-	} else {
-		matchingConfig, ok := findMatchingConfig(indexName, cfg)
-		if !ok {
-			logger.InfoWithCtx(ctx).Msgf("index '%s' is not configured, skipping", indexName)
-			return
-		}
-		if matchingConfig.Enabled {
-			insertCounter.Add(1)
-			if insertCounter.Load()%50 == 1 {
-				logger.DebugWithCtx(ctx).Msgf("%s  --> clickhouse, body(shortened): %s, ctr: %d", indexName, body.ShortString(), insertCounter.Load())
-			}
-			err := action()
-			if err != nil {
-				logger.ErrorWithCtx(ctx).Msg("Can't write to Clickhouse: " + err.Error())
-			}
-		} else {
-			logger.InfoWithCtx(ctx).Msgf("index '%s' is disabled, ignoring", indexName)
-		}
-	}
-}
-
-var matchCounter = atomic.Int32{}
-
-func findMatchingConfig(indexPattern string, cfg config.QuesmaConfiguration) (config.IndexConfiguration, bool) {
-	matchCounter.Add(1)
-	for _, indexConfig := range cfg.IndexConfig {
-		if matchCounter.Load()%100 == 1 {
-			logger.Debug().Msgf("matching index %s with config: %+v, ctr: %d", indexPattern, indexConfig.Name, matchCounter.Load())
-		}
-		if config.MatchName(indexPattern, indexConfig.Name) {
-			if matchCounter.Load()%100 == 1 {
-				logger.Debug().Msgf("  ╚═ matched index %s with config: %+v, ctr: %d", indexPattern, indexConfig.Name, matchCounter.Load())
-			}
-			return indexConfig, true
-		} else {
-			if matchCounter.Load()%100 == 1 {
-				logger.Debug().Msgf("  ╚═ not matched index %s with config: %+v, ctr: %d", indexPattern, indexConfig.Name, matchCounter.Load())
-			}
-		}
-	}
-	return config.IndexConfiguration{}, false
 }
