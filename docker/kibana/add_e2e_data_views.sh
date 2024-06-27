e0dc79e4da5474fb85c500c57782e949613ae24b
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-06-27 12:42:52 +0200

    Cleanup

--- docker/kibana/add_e2e_data_views.sh
+++ docker/kibana/add_e2e_data_views.sh
@@ -11,18 +11,16 @@ add_view() {
   echo -n "Adding data view $view_name"
   do_silent_http_post "api/data_views/data_view" "{
     \"data_view\": {
-      \"name\": \"$view_name\",
+      \"name\":  \"$view_name\",
       \"title\": \"$view_name\",
-      \"id\": \"$view_name\",
+      \"id\":    \"$view_name\",
       \"timeFieldName\": \"timestamp\",
       \"allowNoIndex\": true
     },
     \"override\": true
   }"
+  echo ""
 }
 
-
 add_view "e2e_table_1"
-echo ""
-echo ""
 echo -e "\nData views added."
