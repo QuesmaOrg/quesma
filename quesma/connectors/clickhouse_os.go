39bd776d3daac54b8f01759c5b22bbc1c1f7f341
przemyslaw <przemyslaw.hejman@quesma.com>
2024-07-04 16:23:21 +0200

    sunset check for now

--- quesma/connectors/clickhouse_os.go
+++ quesma/connectors/clickhouse_os.go
@@ -12,7 +12,7 @@ const clickHouseOSConnectorTypeName = "clickhouse-os"
 
 func (c *ClickHouseOSConnector) LicensingCheck() error {
 	// TODO: Check if you're connected to ClickHouse Cloud OR Hydrolix and fail if so
-	return c.Connector.CheckIfConnectedToHydrolix()
+	return nil // return c.Connector.CheckIfConnectedToHydrolix()
 }
 
 func (c *ClickHouseOSConnector) Type() string {
