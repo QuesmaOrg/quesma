6e041ecfdf9416e69ee12d2ad5a29dc4b77b591e
Jacek Migdal <jacek@quesma.com>
2024-07-08 19:57:11 +0200

    Improve bin scripts to save status (#491)
    Currently, there is no notion of the status of docker-compose. So if I
    run:
    ```
    bin/up.sh docker/local-dev-dual-comparison.yml
    ```
    Followed by:
    ```
    bin/restart.sh
    ```
    
    Things gets broken in non-intuitive way.
    
    This fix it by introducing saving docker compose file to status file and
    re-using it if explicit mapping is not provided.

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,21 +1,41 @@
 # Library for common functions and setup
 
 DEFAULT_COMPOSE_FILE="docker/local-dev.yml"
+QUESMA_STATUS_FILE="bin/.running-docker-compose"
+
 : "${QUESMA_COMPOSE_FILE:=$DEFAULT_COMPOSE_FILE}"
 if [ ! -z "$1" ]; then
-  QUESMA_COMPOSE_FILE="$1" # it can be file na,e
-  if [ ! -f "$QUESMA_COMPOSE_FILE" ]; then # orname
+  QUESMA_COMPOSE_FILE="$1" # it can be file path
+  if [ ! -f "$QUESMA_COMPOSE_FILE" ]; then # or name
     QUESMA_COMPOSE_FILE="docker/$1.yml"
   fi
   echo "QUESMA_COMPOSE_FILE set to '$QUESMA_COMPOSE_FILE' by argument '$1'"
 else
   if [ "$QUESMA_COMPOSE_FILE" != "$DEFAULT_COMPOSE_FILE" ]; then
     echo "QUESMA_COMPOSE_FILE overridden by env to $QUESMA_COMPOSE_FILE"
+  else
+    if [ -f "$QUESMA_STATUS_FILE" ]; then
+      QUESMA_COMPOSE_FILE="$(cat $QUESMA_STATUS_FILE)"
+      echo "QUESMA_COMPOSE_FILE set to '$QUESMA_COMPOSE_FILE' from '$QUESMA_STATUS_FILE'"
+    fi
   fi
 fi
 
+remove_status_file() {
+  if [ -f "$QUESMA_STATUS_FILE" ]; then
+    echo "Removing '$QUESMA_STATUS_FILE'"
+    rm $QUESMA_STATUS_FILE
+  fi
+}
+
+create_status_file() {
+  echo "Saving QUESMA_COMPOSE_FILE='$QUESMA_COMPOSE_FILE' to status file '$QUESMA_STATUS_FILE'"
+  echo "$QUESMA_COMPOSE_FILE" > "$QUESMA_STATUS_FILE"
+}
+
 if [ ! -f "$QUESMA_COMPOSE_FILE" ]; then
   echo "File '$QUESMA_COMPOSE_FILE' does not exist."
+  remove_status_file
   exit 1
 fi
 
--- bin/up.sh
+++ bin/up.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0/")/.."
 source bin/lib.sh
 
 docker compose  -f "$QUESMA_COMPOSE_FILE" build --build-arg QUESMA_BUILD_DATE="$QUESMA_BUILD_DATE" --build-arg QUESMA_VERSION="$QUESMA_VERSION" --build-arg QUESMA_BUILD_SHA="$QUESMA_BUILD_SHA" && docker compose -f "$QUESMA_COMPOSE_FILE" up -d
+create_status_file
 
 cat <<"EOF"
                ________
