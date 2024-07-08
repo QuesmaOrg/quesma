39e1b58c3ac04e68bacec15d48a156df23223ae9
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-07-08 15:36:43 +0200

    Script to ingest data from url to Clickhouse

---
+++ bin/ingest-clickhouse/normal_tables.py
@@ -0,0 +1,9 @@
+from table import Table
+import env
+
+NORMAL_TABLES = [table for table in env.TABLES] # if table != env.EXAMPLE_SPECIAL_TABLE]
+
+
+class NormalTable(Table):
+    def __init__(self, table_data, step_by_step):
+        super().__init__(table_data.table_name, table_data.urls, step_by_step)
