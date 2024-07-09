dd59974f5a751e85c6254de36dd3651699eba770
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-07-09 14:37:56 +0200

    Fix build-image.sh (#502)

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # Rebuilds the Quesma Docker image and tags it as quesma:latest
 set -e
-cd "$(dirname "$0/")/.."
-source bin/lib.sh
-
 docker build -f quesma/Dockerfile -t quesma:latest quesma
