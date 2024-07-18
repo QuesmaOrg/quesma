f70435eca1add246a8bac96aa6304fb7e746b4ac
Rafał Strzaliński <rafal@quesma.com>
2024-07-18 19:27:27 +0200

    Optimizer - Rewrite query using a materialized view  (#540)
    This PR adds an optimizer that rewrites queries and replaces the
    original table with a materialized view. The rewrite rule is taken from
    the configuration.
    
    Suppose we've got the following table:
    ```
    CREATE some_index (
      `date` DateTime64(3),
       `type` String,
       ... 
    )
    ENGINE=MergeTree()
    ORDER BY (`date`)
    ```
    
    We've discovered our dashboard generates a lot of queries with common
    expressions:
    ```
    (type ILIKE '%foo%')
    ```
    
    We may create a materialized view and query it instead of the original
    table.
    Notice that the materialized view is updated on INSERT, it must be
    created before ingest.
    ```
    -- a table that holds materialized view data 
    CREATE table some_index_foo_dest (
      `date` DateTime64(3),
       `type` String,
       ... 
    )
    
    -- an index 
    
    CREATE MATERIALIZED VIEW some_index_foo_mv to some_index_foo_dest AS
    SELECT  * 
    FROM some_index 
    WHERE 
    type ILIKE '%foo%'
    ```
    
    Now we can configure the optimizer itself.
    ```
    -- part of config.yaml
     some_index:
        enabled: true
        optimizers:
          materialized_view_replace:
            enabled: true
            properties:
              table: "some_index"
              condition: "(type ILIKE '%foo%')"
              view: "some_index_foo_mv"
    ```
    
    So every query like
    ```
    select count(*) from some_index where ((type ILIKE '%foo%') AND (`date` <= ..... AND `date` >= .....))
    ```
    Will be rewritten to:
    ```
    select count(*) from some_index where (TRUE AND (`date` <= ..... AND `date` >= .....))
    ```
    
    
    Limitations:
    - we can have only one rewrite rule per index/table
    - a condition in the configuration must be the same as a `string`
    representation of the replaced expression
    - setup and configuration are  complex
    
    ---------
    
    Signed-off-by: Rafał Strzaliński <rafal@quesma.com>

--- quesma/quesma/config/config.go
+++ quesma/quesma/config/config.go
@@ -44,7 +44,6 @@ type QuesmaConfiguration struct {
 	PublicTcpPort                 network.Port                          `koanf:"port"`
 	IngestStatistics              bool                                  `koanf:"ingestStatistics"`
 	QuesmaInternalTelemetryUrl    *Url                                  `koanf:"internalTelemetryUrl"`
-	EnabledOptimizers             OptimizersConfiguration               `koanf:"optimizers"`
 	IndexSourceToInternalMappings map[string]IndexMappingsConfiguration `koanf:"indexMappings"`
 }
 
@@ -65,7 +64,10 @@ type RelationalDbConfiguration struct {
 	AdminUrl      *Url   `koanf:"adminUrl"`
 }
 
-type OptimizersConfiguration map[string]bool
+type OptimizerConfiguration struct {
+	Enabled    bool              `koanf:"enabled"`
+	Properties map[string]string `koanf:"properties"`
+}
 
 func (c *RelationalDbConfiguration) IsEmpty() bool {
 	return c != nil && c.Url == nil && c.User == "" && c.Password == "" && c.Database == ""
@@ -241,13 +243,16 @@ func (c *QuesmaConfiguration) WritesToElasticsearch() bool {
 	return c.Mode != ClickHouse
 }
 
-func (c *QuesmaConfiguration) optimizersConfigAsString(s string, cfg OptimizersConfiguration) string {
+func (c *QuesmaConfiguration) optimizersConfigAsString(s string, cfg map[string]OptimizerConfiguration) string {
 
 	var lines []string
 
 	lines = append(lines, fmt.Sprintf("        %s:", s))
 	for k, v := range cfg {
-		lines = append(lines, fmt.Sprintf("            %s: %v", k, v))
+		lines = append(lines, fmt.Sprintf("            %s: %v", k, v.Enabled))
+		if v.Properties != nil && len(v.Properties) > 0 {
+			lines = append(lines, fmt.Sprintf("                properties: %v", v.Properties))
+		}
 	}
 
 	return strings.Join(lines, "\n")
@@ -258,8 +263,6 @@ func (c *QuesmaConfiguration) OptimizersConfigAsString() string {
 	var lines []string
 
 	lines = append(lines, "\n")
-
-	lines = append(lines, c.optimizersConfigAsString("Global", c.EnabledOptimizers))
 
 	for indexName, indexConfig := range c.IndexConfig {
 		if indexConfig.EnabledOptimizers != nil && len(indexConfig.EnabledOptimizers) > 0 {
--- quesma/quesma/config/index_config.go
+++ quesma/quesma/config/index_config.go
@@ -23,8 +23,8 @@ type IndexConfiguration struct {
 	TimestampField *string `koanf:"timestampField"`
 	// this is hidden from the user right now
 	// deprecated
-	SchemaConfiguration *SchemaConfiguration    `koanf:"static-schema"`
-	EnabledOptimizers   OptimizersConfiguration `koanf:"optimizers"`
+	SchemaConfiguration *SchemaConfiguration              `koanf:"static-schema"`
+	EnabledOptimizers   map[string]OptimizerConfiguration `koanf:"optimizers"`
 }
 
 func (c IndexConfiguration) HasFullTextField(fieldName string) bool {
