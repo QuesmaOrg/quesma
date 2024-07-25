cd7c76ec936bc6af3ad2e4d1759637bd15147c55
Przemek Delewski <pdelewski@quesma.com>
2024-07-25 10:14:43 +0200

    Some cleanup

--- quesma/plugins/ingest_validator/ingest_validator.go
+++ quesma/plugins/ingest_validator/ingest_validator.go
@@ -9,6 +9,7 @@ import (
 	"quesma/quesma/config"
 	"quesma/quesma/types"
 	"quesma/schema"
+	"quesma/util"
 	"reflect"
 )
 
@@ -19,38 +20,6 @@ type IngestValidator struct {
 	tableMap       clickhouse.TableMap
 }
 
-/*
-	func isLong(f float64) bool {
-		return f == float64(int64(f))
-	}
-
-	func isUnsignedLong(f float64) bool {
-		if f < 0 {
-			return false
-		}
-		return f == float64(uint64(f))
-	}
-*/
-func getTypeName(v interface{}) string {
-	t := reflect.TypeOf(v).String()
-	/*
-		switch t {
-		case "string":
-			return "text"
-		case "bool":
-			return "boolean"
-		case "float64":
-			if isLong(v.(float64)) {
-				return "long"
-			} else if isUnsignedLong(v.(float64)) {
-				return "unsigned_long"
-			} else {
-				return "float"
-			}
-		}*/
-	return t
-}
-
 func (iv *IngestValidator) Transform(document types.JSON) (types.JSON, error) {
 	clickhouseTable, ok := iv.tableMap.Load(iv.table)
 	if !ok {
@@ -59,9 +28,15 @@ func (iv *IngestValidator) Transform(document types.JSON) (types.JSON, error) {
 	}
 
 	for k, v := range document {
-		if clickhouseTable.Cols[k].Type.String() != getTypeName(v) {
-			logger.Error().Msgf("Field %s has wrong type %s, expected %s", k, getTypeName(v), clickhouseTable.Cols[k].Type.String())
-			return document, nil
+		if v != nil {
+			column := clickhouseTable.Cols[k]
+			if column != nil {
+				kind, _ := util.KindFromString(column.Type.String())
+				if kind != reflect.TypeOf(v).Kind() {
+					//logger.Error().Msgf("Field %s has wrong type %s, expected %s", k, getTypeName(v), clickhouseTable.Cols[k].Type.String())
+					return document, nil
+				}
+			}
 		}
 	}
 	return document, nil
