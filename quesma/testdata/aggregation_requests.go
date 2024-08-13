c31f93b08e43a2a9bbbe142b6b5bccfc190baee9
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-08-13 20:39:00 +0200

    Fix 1 test with wrong answers

--- quesma/testdata/aggregation_requests.go
+++ quesma/testdata/aggregation_requests.go
@@ -2570,12 +2570,14 @@ var AggregationTests = []AggregationTestCase{
 				  "aggr__sample__top_values__count", count() AS
 				  "aggr__sample__top_values__order_1", count(*) AS
 				  "aggr__sample__count_part"
-				FROM "logs-generic-default"
-				WHERE (("@timestamp">=parseDateTime64BestEffort('2024-01-23T11:27:16.820Z')
-				  AND "@timestamp"<=parseDateTime64BestEffort('2024-01-23T11:42:16.820Z'))
-				  AND "message" iLIKE '%user%')
+				FROM (
+				  SELECT "host.name"
+				  FROM "logs-generic-default"
+				  WHERE (("@timestamp">=parseDateTime64BestEffort('2024-01-23T11:27:16.820Z')
+				    AND "@timestamp"<=parseDateTime64BestEffort('2024-01-23T11:42:16.820Z')) AND "message" iLIKE '%user%')
+				  LIMIT 8000)
 				GROUP BY "host.name" AS "aggr__sample__top_values__key_0"))
-			WHERE "aggr__sample__top_values__order_1_rank"<=10
+			WHERE "aggr__sample__top_values__order_1_rank"<=11
 			ORDER BY "aggr__sample__top_values__order_1_rank" ASC`,
 	},
 	{ // [12], "old" test, also can be found in testdata/requests.go TestAsyncSearch[3]
