5e7fda800a229f55854cde2e384d51de4fc89218
Rafal Strzalinski <rafal@quesma.com>
2024-08-26 17:52:49 +0200

    License

--- quesma/model/constants.go
+++ quesma/model/constants.go
@@ -1,9 +1,7 @@
+// Copyright Quesma, licensed under the Elastic License 2.0.
+// SPDX-License-Identifier: Elastic-2.0
 package model
 
 const (
 	SingleTableNamePlaceHolder = "__quesma_table_name"
-
-	TimestampColumnName = "__quesma_timestamp_column"
-
-	TimestampGroupBySecondsFunctionName = "__quesma_timestamp_group_by_seconds"
 )
