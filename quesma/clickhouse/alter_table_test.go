fdd2b1173239e75d8fe654b6c59fcf2c3851f7e2
Przemyslaw Delewski <102958445+pdelewski@users.noreply.github.com>
2024-08-26 15:16:35 +0200

    Alter column heuristic (#663)
    This PR: 
    - enables altering column mechanism as default
    - introduce simple heuristic (first N columns is added by default, next
    columns are added based on simple statistic calculation)
    - opens a way for changing/extending current heuristic

--- quesma/clickhouse/alter_table_test.go
+++ quesma/clickhouse/alter_table_test.go
@@ -7,6 +7,7 @@ import (
 	"quesma/concurrent"
 	"quesma/quesma/config"
 	"quesma/quesma/types"
+	"strconv"
 	"testing"
 )
 
@@ -45,7 +46,7 @@ func TestAlterTable(t *testing.T) {
 
 	lm := NewLogManager(fieldsMap, config.QuesmaConfiguration{})
 	for i := range rowsToInsert {
-		insert, alter, err := lm.BuildIngestSQLStatements("tableName", types.MustJSON(rowsToInsert[i]), nil, chConfig, true)
+		insert, alter, err := lm.BuildIngestSQLStatements("tableName", types.MustJSON(rowsToInsert[i]), nil, chConfig)
 		assert.Equal(t, expectedInsert[i], insert)
 		assert.Equal(t, alters[i], alter[0])
 		// Table will grow with each iteration
@@ -61,4 +62,56 @@ func TestAlterTable(t *testing.T) {
 
 		assert.NoError(t, err)
 	}
+}
+
+func TestAlterTableHeuristic(t *testing.T) {
+	chConfig := &ChTableConfig{
+		hasTimestamp:         true,
+		timestampDefaultsNow: true,
+		engine:               "MergeTree",
+		orderBy:              "(timestamp)",
+		partitionBy:          "",
+		primaryKey:           "",
+		ttl:                  "",
+		attributes: []Attribute{
+			NewDefaultStringAttribute(),
+		},
+		castUnsupportedAttrValueTypesToString: true,
+		preferCastingToOthers:                 true,
+	}
+	table := &Table{
+		Cols: map[string]*Column{},
+	}
+	const tableName = "tableName"
+	fieldsMap := concurrent.NewMapWith(tableName, table)
+	lm := NewLogManager(fieldsMap, config.QuesmaConfiguration{})
+
+	rowsToInsert := make([]string, 0)
+	previousRow := ``
+	comma := ``
+	const numberOfInserts = 1000
+	for i := range numberOfInserts {
+		if i > 0 {
+			comma = ","
+		}
+		currentRow := previousRow + comma + `"Test` + strconv.Itoa(i) + `":` + strconv.Itoa(i)
+		rowsToInsert = append(rowsToInsert, `{`+currentRow+`}`)
+		previousRow = currentRow
+	}
+
+	assert.Equal(t, int64(0), lm.ingestCounter)
+	for i := range rowsToInsert {
+		_, _, err := lm.BuildIngestSQLStatements(tableName, types.MustJSON(rowsToInsert[i]), nil, chConfig)
+		assert.NoError(t, err)
+
+	}
+	// Total number of columns should be 500
+	// First 100 columns will be added in the first 100 inserts
+	// without checking any additional heuristics
+	// The rest of the columns will be added in the next 900 inserts
+	// using the heuristic
+	const expectedNumberOfColumns = 500
+	assert.Equal(t, len(table.Cols), expectedNumberOfColumns)
+
+	assert.Equal(t, int64(numberOfInserts), lm.ingestCounter)
 }
