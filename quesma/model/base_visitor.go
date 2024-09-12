60267ba71f1296307ca9182a6c51e681437cf8f2
Jacek Migdal <jacek@quesma.com>
2024-09-12 15:22:41 +0200

    Remove ExchangeToAliasInCTE

--- quesma/model/base_visitor.go
+++ quesma/model/base_visitor.go
@@ -102,7 +102,7 @@ func (v *BaseExprVisitor) VisitOrderByExpr(e OrderByExpr) interface{} {
 	if v.OverrideVisitOrderByExpr != nil {
 		return v.OverrideVisitOrderByExpr(v, e)
 	}
-	return OrderByExpr{Expr: e.Expr.Accept(v).(Expr), Direction: e.Direction, ExchangeToAliasInCTE: e.ExchangeToAliasInCTE}
+	return OrderByExpr{Expr: e.Expr.Accept(v).(Expr), Direction: e.Direction}
 }
 
 func (v *BaseExprVisitor) VisitDistinctExpr(e DistinctExpr) interface{} {
