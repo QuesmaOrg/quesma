ac8a01f5229b720aef6cb1fa7f19a02e03866af5
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-09-23 09:54:49 +0200

    Very small code cleanup (#781)
    Just a small removal of some pre-pancake code (+ fix expected results
    for 1 test)

--- quesma/model/bucket_aggregations/terms.go
+++ quesma/model/bucket_aggregations/terms.go
@@ -7,7 +7,6 @@ import (
 	"quesma/logger"
 	"quesma/model"
 	"quesma/util"
-	"strings"
 )
 
 type Terms struct {
@@ -48,21 +47,14 @@ func (query Terms) TranslateSqlResponseToJson(rows []model.QueryResultRow) model
 	}
 
 	if !query.significant {
-		sumOtherDocCount := 0
-		if query.isPancake(rows[0]) { // TODO: remove this after change to pancake-only
-			sumOtherDocCount = int(util.ExtractInt64(query.parentCount(rows[0]))) - query.sumDocCounts(rows)
-		}
-
+		sumOtherDocCount := int(util.ExtractInt64(query.parentCount(rows[0]))) - query.sumDocCounts(rows)
 		return model.JsonMap{
 			"sum_other_doc_count":         sumOtherDocCount,
 			"doc_count_error_upper_bound": 0,
 			"buckets":                     response,
 		}
 	} else {
-		var parentDocCount int64
-		if query.isPancake(rows[0]) { // TODO: remove this after change to pancake-only
-			parentDocCount = util.ExtractInt64(query.parentCount(rows[0]))
-		}
+		parentDocCount := util.ExtractInt64(query.parentCount(rows[0]))
 		return model.JsonMap{
 			"buckets":   response,
 			"doc_count": parentDocCount,
@@ -108,12 +100,4 @@ func (query Terms) key(row model.QueryResultRow) any {
 
 func (query Terms) parentCount(row model.QueryResultRow) any {
 	return row.Cols[len(row.Cols)-3].Value
-}
-
-// Soon-to-be deprecated. TODO: Remove after change to pancake-only
-func (query Terms) isPancake(row model.QueryResultRow) bool {
-	if len(row.Cols) < 3 {
-		return false
-	}
-	return strings.HasSuffix(row.Cols[len(row.Cols)-3].ColName, "parent_count")
 }
--- quesma/testdata/aggregation_requests.go
+++ quesma/testdata/aggregation_requests.go
@@ -1175,7 +1175,7 @@ var AggregationTests = []AggregationTestCase{
 										}
 									],
 									"doc_count_error_upper_bound": 0,
-									"sum_other_doc_count": 0
+									"sum_other_doc_count": 250
 								},
 								"doc_count": 283,
 								"key": "UIO",
@@ -1205,7 +1205,7 @@ var AggregationTests = []AggregationTestCase{
 							},
 							{
 								"distinations": {
-									"sum_other_doc_count": 0,
+									"sum_other_doc_count": 4,
 									"buckets": [
 										{
 											"destLocation": {
@@ -1261,7 +1261,7 @@ var AggregationTests = []AggregationTestCase{
 										}
 									]
 								},
-								"doc_count": 15,
+								"doc_count": 25,
 								"key": "DLH",
 								"originLocation": {
 									"hits": {
@@ -1289,7 +1289,7 @@ var AggregationTests = []AggregationTestCase{
 							}
 						],
 						"doc_count_error_upper_bound": 0,
-						"sum_other_doc_count": 12716
+						"sum_other_doc_count": 12706
 					}
 				},
 				"hits": {
@@ -1311,9 +1311,9 @@ var AggregationTests = []AggregationTestCase{
 					model.NewQueryResultCol("aggr__origins__parent_count", uint64(13014)),
 					model.NewQueryResultCol("aggr__origins__key_0", "UIO"),
 					model.NewQueryResultCol("aggr__origins__count", int64(283)),
+					model.NewQueryResultCol("aggr__origins__distinations__parent_count", int64(283)),
 					model.NewQueryResultCol("aggr__origins__distinations__key_0", "EZE"),
 					model.NewQueryResultCol("aggr__origins__distinations__count", int64(21)),
-					model.NewQueryResultCol("aggr__origins__distinations__order_1", int64(21)),
 					model.NewQueryResultCol("top_hits__origins__distinations__destLocation_col_0", map[string]interface{}{
 						"lat": "-34.8222",
 						"lon": "-58.5358",
@@ -1326,9 +1326,9 @@ var AggregationTests = []AggregationTestCase{
 					model.NewQueryResultCol("aggr__origins__parent_count", uint64(13014)),
 					model.NewQueryResultCol("aggr__origins__key_0", "UIO"),
 					model.NewQueryResultCol("aggr__origins__count", int64(283)),
+					model.NewQueryResultCol("aggr__origins__distinations__parent_count", int64(283)),
 					model.NewQueryResultCol("aggr__origins__distinations__key_0", "UIO"),
 					model.NewQueryResultCol("aggr__origins__distinations__count", int64(12)),
-					model.NewQueryResultCol("aggr__origins__distinations__order_1", int64(12)),
 					model.NewQueryResultCol("top_hits__origins__distinations__destLocation_col_0", map[string]interface{}{
 						"lat": "-0.129166667",
 						"lon": "-78.3575",
@@ -1340,10 +1340,10 @@ var AggregationTests = []AggregationTestCase{
 				Cols: []model.QueryResultCol{
 					model.NewQueryResultCol("aggr__origins__parent_count", uint64(13014)),
 					model.NewQueryResultCol("aggr__origins__key_0", "DLH"),
-					model.NewQueryResultCol("aggr__origins__count", int64(15)),
+					model.NewQueryResultCol("aggr__origins__count", int64(25)),
+					model.NewQueryResultCol("aggr__origins__distinations__parent_count", int64(25)),
 					model.NewQueryResultCol("aggr__origins__distinations__key_0", "YUL"),
 					model.NewQueryResultCol("aggr__origins__distinations__count", int64(11)),
-					model.NewQueryResultCol("aggr__origins__distinations__order_1", int64(11)),
 					model.NewQueryResultCol("top_hits__origins__distinations__destLocation_col_0", map[string]interface{}{
 						"lat": "45.47060013",
 						"lon": "-73.74079895",
@@ -1355,10 +1355,10 @@ var AggregationTests = []AggregationTestCase{
 				Cols: []model.QueryResultCol{
 					model.NewQueryResultCol("aggr__origins__parent_count", uint64(13014)),
 					model.NewQueryResultCol("aggr__origins__key_0", "DLH"),
-					model.NewQueryResultCol("aggr__origins__count", int64(15)),
+					model.NewQueryResultCol("aggr__origins__count", int64(25)),
+					model.NewQueryResultCol("aggr__origins__distinations__parent_count", int64(25)),
 					model.NewQueryResultCol("aggr__origins__distinations__key_0", "EZE"),
 					model.NewQueryResultCol("aggr__origins__distinations__count", int64(10)),
-					model.NewQueryResultCol("aggr__origins__distinations__order_1", int64(10)),
 					model.NewQueryResultCol("top_hits__origins__distinations__destLocation_col_0", map[string]interface{}{
 						"lat": "-34.8222",
 						"lon": "-58.5358",
