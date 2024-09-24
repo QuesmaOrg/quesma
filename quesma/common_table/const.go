021d411a776a9925b5d05a200ba08b55981e65f6
Rafal Strzalinski <rafal@quesma.com>
2024-09-24 17:30:31 +0200

    Add versioning to virtual table definitions

--- quesma/common_table/const.go
+++ quesma/common_table/const.go
@@ -41,13 +41,19 @@ func EnsureCommonTableExists(db *sql.DB) {
 
 // Here are defintion of JSON objects that are used to store virtual tables in JSON database
 
+
+const VirtualTableElasticIndexName = "quesma_virtual_tables"
+const VirtualTableStructVersion = "1"
+
 type VirtualTableColumn struct {
 	Name    string `json:"name"`
 	Type    string `json:"type"`
 	Comment string `json:"comment"`
 }
 
+
 type VirtualTable struct {
+	Version  string               `json:"version"`
 	StoredAt string               `json:"stored_at"`
 	Columns  []VirtualTableColumn `json:"columns"`
 }
