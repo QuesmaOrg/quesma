e44043220b034fadff7e2ce98557d2d842c346b2
Rafal Strzalinski <rafal@quesma.com>
2024-09-25 15:14:17 +0200

    Tests

---
+++ quesma/comment_metadata/comment_metadata_test.go
@@ -0,0 +1,96 @@
+// Copyright Quesma, licensed under the Elastic License 2.0.
+// SPDX-License-Identifier: Elastic-2.0
+package comment_metadata
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCommentMetadata_Marshall(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		input map[string]string
+		want  string
+	}{
+		{
+			name: "test1",
+			input: map[string]string{
+				"foo": "bar",
+			},
+			want: "quesmaMetadata:foo=bar&v=1",
+		},
+		{
+			name: "test2",
+			input: map[string]string{
+				"łąś": "żółć",
+			},
+			want: "quesmaMetadata:v=1&%C5%82%C4%85%C5%9B=%C5%BC%C3%B3%C5%82%C4%87",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cm := NewCommentMetadata()
+			cm.Values = tt.input
+
+			if got := cm.Marshall(); got != tt.want {
+				t.Errorf("Marshall() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUnmarshallCommentMetadata(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		input string
+		want  map[string]string
+		fail  bool
+	}{
+		{
+			name:  "simple",
+			input: "quesmaMetadata:foo=bar&v=1",
+			want: map[string]string{
+				"foo": "bar",
+			},
+			fail: false,
+		},
+		{
+			name:  "with special characters",
+			input: "quesmaMetadata:v=1&%C5%82%C4%85%C5%9B=%C5%BC%C3%B3%C5%82%C4%87",
+			want: map[string]string{
+				"łąś": "żółć",
+			},
+			fail: false,
+		},
+		{
+			name:  "with human comments ",
+			input: "some comment here  quesmaMetadata:foo=bar&v=1  and here ",
+			want: map[string]string{
+				"foo": "bar",
+			},
+			fail: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cm, err := UnmarshallCommentMetadata(tt.input)
+
+			if tt.fail {
+				if err == nil {
+					t.Fatal("Expecting error, got nil")
+				}
+			} else {
+				if err != nil {
+					t.Fatal("Unexpected error ", err)
+				}
+			}
+
+			assert.Equal(t, tt.want, cm.Values)
+		})
+	}
+}
