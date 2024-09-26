f8b77012b4099866e909c1a8127d4f070acb1948
Przemysław Hejman <przemyslaw.hejman@quesma.com>
2024-09-26 11:58:41 +0200

    Bring back transparent proxy mode (#803)
    Co-authored-by: Krzysztof Kiewicz <krzysztof@quesma.com>

--- quesma/quesma/config/config_v2.go
+++ quesma/quesma/config/config_v2.go
@@ -505,6 +505,7 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 
 		if queryProcessor.Type == QuesmaV1ProcessorNoOp && ingestProcessor.Type == QuesmaV1ProcessorNoOp {
 			conf.TransparentProxy = true
+			goto END
 		}
 
 		elasticBackendName := c.getElasticsearchBackendConnector().Name
@@ -571,6 +572,7 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 		delete(conf.IndexConfig, DefaultWildcardIndexName)
 	}
 
+END:
 	if errAcc != nil {
 		var multiErr *multierror.Error
 		if errors.As(errAcc, &multiErr) {
