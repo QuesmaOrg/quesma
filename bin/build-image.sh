5d79ff62709ae53afc73389c00322ac13db261d8
Piotr Grabowski <avelanarius@gmail.com>
2024-10-03 12:21:57 +0200

    Update bin/build-image.sh
    Signed-off-by: Piotr Grabowski <avelanarius@gmail.com>

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Rebuilds the Quesma Docker image and tags it as quesma:latest
+# Rebuilds the Quesma Docker image and tags it as quesma:nightly
 set -e
 cd "$(dirname "$0/")/.."
 docker build -f quesma/Dockerfile -t quesma/quesma:nightly quesma
