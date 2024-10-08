22928bd0081b577d14f0dfb74ea59a3ec7771dbd
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-10-08 21:09:18 +0200

    Async_search persistent storage 1 (#858)
    Probably first of 2 PRs.
    
    I didn't plan on changing **anything** in the logic. I just created a
    common interface, which is implemented by our old in-memory storage.
    
    In the second PR I'll implement `in_elastic.go` and start using it at
    Quesma creation instead of `in_memory.go`. The interface will likely
    change, or might not change, I don't know yet. AFAIK `in_elastic`
    version is vastly preferred, so if I see some change in the interface
    which might be better suited for it, I'll probably go for it.
    
    Any suggestions considering naming, etc. are very welcomed. We can also
    wait for it till the second PR, maybe then some better namings will come
    out naturally.

--- quesma/main.go
+++ quesma/main.go
@@ -21,6 +21,7 @@ import (
 	"quesma/logger"
 	"quesma/persistence"
 	"quesma/quesma"
+	"quesma/quesma/async_search_storage"
 	"quesma/quesma/config"
 	"quesma/quesma/ui"
 	"quesma/schema"
@@ -74,7 +75,7 @@ func main() {
 	}()
 
 	if asyncQueryTraceLogger != nil {
-		asyncQueryTraceEvictor := quesma.AsyncQueryTraceLoggerEvictor{AsyncQueryTrace: asyncQueryTraceLogger.AsyncQueryTrace}
+		asyncQueryTraceEvictor := async_search_storage.AsyncQueryTraceLoggerEvictor{AsyncQueryTrace: asyncQueryTraceLogger.AsyncQueryTrace}
 		asyncQueryTraceEvictor.Start()
 		defer asyncQueryTraceEvictor.Stop()
 	}
@@ -135,6 +136,6 @@ func constructQuesma(cfg *config.QuesmaConfiguration, sl clickhouse.TableDiscove
 	if cfg.TransparentProxy {
 		return quesma.NewQuesmaTcpProxy(phoneHomeAgent, cfg, quesmaManagementConsole, logChan, false)
 	} else {
-		return quesma.NewHttpProxy(phoneHomeAgent, lm, ip, sl, im, schemaRegistry, cfg, quesmaManagementConsole, logChan, abResultsrepository)
+		return quesma.NewHttpProxy(phoneHomeAgent, lm, ip, sl, im, schemaRegistry, cfg, quesmaManagementConsole, abResultsrepository)
 	}
 }
