e72b4aa450fe4255ffa5bee1c49a108a3fb2a4f1
przemyslaw <przemyslaw.hejman@quesma.com>
2024-10-11 15:29:44 +0200

    COMPLETE SET

--- ci/it/testcases/test_dual_write_and_common_table.go
+++ ci/it/testcases/test_dual_write_and_common_table.go
@@ -33,7 +33,7 @@ func (a *DualWriteAndCommonTableTestcase) SetupContainers(ctx context.Context) e
 func (a *DualWriteAndCommonTableTestcase) RunTests(ctx context.Context, t *testing.T) error {
 	a.testBasicRequest(ctx, t)
 	a.testIngestToClickHouseWorks(ctx, t)
-	//a.testIngestToCommonTableWorks(ctx, t)
+	a.testIngestToCommonTableWorks(ctx, t)
 	a.testDualQueryReturnsDataFromClickHouse(ctx, t)
 	a.testDualWritesWork(ctx, t)
 	a.testWildcardGoesToElastic(ctx, t)
@@ -47,6 +47,70 @@ func (a *DualWriteAndCommonTableTestcase) testBasicRequest(ctx context.Context, 
 	}
 	defer resp.Body.Close()
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+func (a *DualWriteAndCommonTableTestcase) testIngestToCommonTableWorks(ctx context.Context, t *testing.T) {
+	resp, err := a.RequestToQuesma(ctx, "POST", "/logs-4/_doc", []byte(`{"name": "Przemyslaw", "age": 31337}`))
+	if err != nil {
+		t.Fatalf("Failed to insert document: %s", err)
+	}
+	defer resp.Body.Close()
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	chQuery := "SELECT * FROM 'quesma_common_table'"
+	rows, err := a.ExecuteClickHouseQuery(ctx, chQuery)
+	if err != nil {
+		t.Fatalf("Failed to execute query: %s", err)
+	}
+	columnTypes, err := rows.ColumnTypes()
+	values := make([]interface{}, len(columnTypes))
+	valuePtrs := make([]interface{}, len(columnTypes))
+	for i := range values {
+		valuePtrs[i] = &values[i]
+	}
+	var name string
+	var age int
+	var quesmaIndexName string
+	for rows.Next() {
+		if err := rows.Scan(valuePtrs...); err != nil {
+			t.Fatalf("Failed to scan row: %s", err)
+		}
+		for i, col := range values {
+			switch columnTypes[i].Name() {
+			case "__quesma_index_name":
+				if v, ok := col.(string); ok {
+					quesmaIndexName = v
+				}
+			case "name":
+				if v, ok := col.(*string); ok {
+					name = *v
+				}
+			case "age":
+				if v, ok := col.(*int64); ok {
+					age = int(*v)
+				}
+			}
+		}
+		if name == "Przemyslaw" && age == 31337 && quesmaIndexName == "logs-4" {
+			break
+		}
+	}
+	assert.Equal(t, "Przemyslaw", name)
+	assert.Equal(t, 31337, age)
+	assert.Equal(t, "logs-4", quesmaIndexName)
+
+	resp, err = a.RequestToQuesma(ctx, "GET", "/logs-4/_search", []byte(`{"query": {"match_all": {}}}`))
+	if err != nil {
+		t.Fatalf("Failed to make GET request: %s", err)
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to read response body: %s", err)
+	}
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Contains(t, string(bodyBytes), "Przemyslaw")
+	assert.Contains(t, "Clickhouse", resp.Header.Get("X-Quesma-Source"))
 }
 
 func (a *DualWriteAndCommonTableTestcase) testDualQueryReturnsDataFromClickHouse(ctx context.Context, t *testing.T) {
