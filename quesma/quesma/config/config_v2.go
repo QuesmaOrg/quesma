QuesmaOrg/quesma

commit b520acafb2e2a91ee91b51d4cfe058a4d081c54f

Allow more configurations of targets for "*" (#905)
Previously, a user was allowed to only put ElasticSearch connector as a
target for `*` index configuration (describing the default/fallback
behavior). This PR lifts that requirement and allows for any connectors
to be put into ingest's `*` and at most any 1 connector into query's
`*`.

`table_resolver` code is modified to now read the `*` configuration and
use it for decision (instead of always falling back to Elastic).

This PR also adds back enabling autodiscovery via configuration - if
`ClickHouse` target is listed in `*` targets, autodiscovery will be
turned on.

quesma/quesma/config/config_v2.go
@@ -107,11 +107,6 @@ type Processor struct {
 	Config QuesmaProcessorConfig `koanf:"config"`
 }
 
-var (
-	DefaultIngestTarget = []string{ElasticsearchTarget}
-	DefaultQueryTarget  = []string{ElasticsearchTarget}
-)
-
 // An index configuration under this name in IndexConfig
 // specifies the default configuration for all (non-configured) indexes
 const DefaultWildcardIndexName = "*"
@@ -547,6 +542,24 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 			// TODO refactor this to a separate function
 
 			conf.IndexConfig = make(map[string]IndexConfiguration)
+
+			// Handle default index configuration
+			defaultConfig := queryProcessor.Config.IndexConfig[DefaultWildcardIndexName]
+			for _, target := range defaultConfig.Target {
+				if targetType, found := c.getTargetType(target); found {
+					defaultConfig.QueryTarget = append(defaultConfig.QueryTarget, targetType)
+				} else {
+					errAcc = multierror.Append(errAcc, fmt.Errorf("invalid target %s in configuration of %s", target, DefaultWildcardIndexName))
+				}
+			}
+			if len(defaultConfig.QueryTarget) > 1 {
+				errAcc = multierror.Append(errAcc, fmt.Errorf("the target configuration of default index ('%s') of query processor is not currently supported", DefaultWildcardIndexName))
+			}
+			conf.DefaultIngestTarget = []string{}
+			conf.DefaultQueryTarget = defaultConfig.QueryTarget
+			conf.AutodiscoveryEnabled = slices.Contains(conf.DefaultQueryTarget, ClickhouseTarget)
+			delete(queryProcessor.Config.IndexConfig, DefaultWildcardIndexName)
+
 			for indexName, indexConfig := range queryProcessor.Config.IndexConfig {
 				processedConfig := indexConfig
 				processedConfig.Name = indexName
@@ -575,13 +588,6 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 
 				conf.IndexConfig[indexName] = processedConfig
 			}
-
-			// Handle default index configuration
-			defaultConfig := conf.IndexConfig[DefaultWildcardIndexName]
-			if !reflect.DeepEqual(defaultConfig.QueryTarget, []string{ElasticsearchTarget}) {
-				errAcc = multierror.Append(errAcc, fmt.Errorf("the target configuration of default index ('%s') of query processor is not currently supported", DefaultWildcardIndexName))
-			}
-			delete(conf.IndexConfig, DefaultWildcardIndexName)
 		} else {
 			errAcc = multierror.Append(errAcc, fmt.Errorf("unsupported processor %s in single pipeline", procType))
 		}
@@ -603,11 +609,38 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 		}
 
 		conf.IndexConfig = make(map[string]IndexConfiguration)
+
+		// Handle default index configuration
+		defaultConfig := queryProcessor.Config.IndexConfig[DefaultWildcardIndexName]
+		for _, target := range defaultConfig.Target {
+			if targetType, found := c.getTargetType(target); found {
+				defaultConfig.QueryTarget = append(defaultConfig.QueryTarget, targetType)
+			} else {
+				errAcc = multierror.Append(errAcc, fmt.Errorf("invalid target %s in configuration of %s", target, DefaultWildcardIndexName))
+			}
+		}
+		for _, target := range ingestProcessor.Config.IndexConfig[DefaultWildcardIndexName].Target {
+			if targetType, found := c.getTargetType(target); found {
+				defaultConfig.IngestTarget = append(defaultConfig.IngestTarget, targetType)
+			} else {
+				errAcc = multierror.Append(errAcc, fmt.Errorf("invalid target %s in configuration of %s", target, DefaultWildcardIndexName))
+			}
+		}
+		if len(defaultConfig.QueryTarget) > 1 {
+			errAcc = multierror.Append(errAcc, fmt.Errorf("the target configuration of default index ('%s') of query processor is not currently supported", DefaultWildcardIndexName))
+		}
+		// No restrictions for ingest target!
+		conf.DefaultIngestTarget = defaultConfig.IngestTarget
+		conf.DefaultQueryTarget = defaultConfig.QueryTarget
+		conf.AutodiscoveryEnabled = slices.Contains(conf.DefaultQueryTarget, ClickhouseTarget)
+		delete(queryProcessor.Config.IndexConfig, DefaultWildcardIndexName)
+		delete(ingestProcessor.Config.IndexConfig, DefaultWildcardIndexName)
+
 		for indexName, indexConfig := range queryProcessor.Config.IndexConfig {
 			processedConfig := indexConfig
 			processedConfig.Name = indexName
 
-			processedConfig.IngestTarget = DefaultIngestTarget
+			processedConfig.IngestTarget = defaultConfig.IngestTarget
 
 			for _, target := range indexConfig.Target {
 				if targetType, found := c.getTargetType(target); found {
@@ -643,10 +676,10 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 				// use the ingest processor's configuration as the base (similarly as in the previous loop)
 				processedConfig = indexConfig
 				processedConfig.Name = indexName
-				processedConfig.QueryTarget = DefaultQueryTarget
+				processedConfig.QueryTarget = defaultConfig.QueryTarget
 			}
 
-			processedConfig.IngestTarget = make([]string, 0) // reset previously set DefaultIngestTarget
+			processedConfig.IngestTarget = make([]string, 0) // reset previously set defaultConfig.IngestTarget
 			for _, target := range indexConfig.Target {
 				if targetType, found := c.getTargetType(target); found {
 					processedConfig.IngestTarget = append(processedConfig.IngestTarget, targetType)
@@ -657,16 +690,6 @@ func (c *QuesmaNewConfiguration) TranslateToLegacyConfig() QuesmaConfiguration {
 
 			conf.IndexConfig[indexName] = processedConfig
 		}
-
-		// Handle default index configuration
-		defaultConfig := conf.IndexConfig[DefaultWildcardIndexName]
-		if !reflect.DeepEqual(defaultConfig.QueryTarget, []string{ElasticsearchTarget}) {
-			errAcc = multierror.Append(errAcc, fmt.Errorf("the target configuration of default index ('%s') of query processor is not currently supported", DefaultWildcardIndexName))
-		}
-		if !reflect.DeepEqual(defaultConfig.IngestTarget, []string{ElasticsearchTarget}) {
-			errAcc = multierror.Append(errAcc, fmt.Errorf("the target configuration of default index ('%s') of ingest processor is not currently supported", DefaultWildcardIndexName))
-		}
-		delete(conf.IndexConfig, DefaultWildcardIndexName)
 	}
 
 END:

quesma/table_resolver/rules.go
@@ -57,12 +57,43 @@ func resolveInternalElasticName(pattern parsedPattern) *Decision {
 	return nil
 }
 
-func makeElasticIsDefault(cfg map[string]config.IndexConfiguration) func(input parsedPattern) *Decision {
-
+func makeDefaultWildcard(quesmaConf config.QuesmaConfiguration, pipeline string) func(input parsedPattern) *Decision {
 	return func(input parsedPattern) *Decision {
+		var targets []string
+		var useConnectors []ConnectorDecision
+
+		switch pipeline {
+		case IngestPipeline:
+			targets = quesmaConf.DefaultIngestTarget
+		case QueryPipeline:
+			targets = quesmaConf.DefaultQueryTarget
+		default:
+			return &Decision{
+				Reason: "Unsupported configuration",
+				Err:    end_user_errors.ErrSearchCondition.New(fmt.Errorf("unsupported pipeline: %s", pipeline)),
+			}
+		}
+
+		for _, target := range targets {
+			switch target {
+			case config.ClickhouseTarget:
+				useConnectors = append(useConnectors, &ConnectorDecisionClickhouse{
+					ClickhouseTableName: input.source,
+					ClickhouseTables:    []string{input.source},
+				})
+			case config.ElasticsearchTarget:
+				useConnectors = append(useConnectors, &ConnectorDecisionElastic{})
+			default:
+				return &Decision{
+					Reason: "Unsupported configuration",
+					Err:    end_user_errors.ErrSearchCondition.New(fmt.Errorf("unsupported target: %s", target)),
+				}
+			}
+		}
+
 		return &Decision{
-			UseConnectors: []ConnectorDecision{&ConnectorDecisionElastic{}},
-			Reason:        "Elastic is default.",
+			UseConnectors: useConnectors,
+			Reason:        fmt.Sprintf("Using default wildcard ('%s') configuration for %s processor", config.DefaultWildcardIndexName, pipeline),
 		}
 	}
 }

quesma/quesma/config/config.go
@@ -46,6 +46,9 @@ type QuesmaConfiguration struct {
 
 	EnableIngest      bool // this is computed from the configuration 2.0
 	CreateCommonTable bool
+
+	DefaultIngestTarget []string
+	DefaultQueryTarget  []string
 }
 
 func (c *QuesmaConfiguration) AliasFields(indexName string) map[string]string {

quesma/quesma/config/util.go
@@ -1,65 +1,0 @@
-// Copyright Quesma, licensed under the Elastic License 2.0.
-// SPDX-License-Identifier: Elastic-2.0
-package config
-
-import (
-	"context"
-	"quesma/logger"
-	"quesma/quesma/types"
-	"sync/atomic"
-)
-
-var insertCounter = atomic.Int32{}
-
-func RunConfiguredIngest(ctx context.Context, cfg *QuesmaConfiguration, indexName string, body types.JSON, action func() error) error {
-	if len(cfg.IndexConfig) == 0 {
-		logger.InfoWithCtx(ctx).Msgf("%s  --> clickhouse, body(shortened): %s", indexName, body.ShortString())
-		err := action()
-		if err != nil {
-			logger.ErrorWithCtx(ctx).Msg("Can't write to index: " + err.Error())
-		}
-		return err
-	} else {
-		matchingConfig, ok := findMatchingConfig(indexName, cfg)
-		if !ok {
-			logger.InfoWithCtx(ctx).Msgf("index '%s' is not configured, skipping", indexName)
-			return nil
-		}
-		if !matchingConfig.IsClickhouseIngestEnabled() {
-			logger.InfoWithCtx(ctx).Msgf("index '%s' is disabled, ignoring", indexName)
-			return nil
-		} else {
-			insertCounter.Add(1)
-			if insertCounter.Load()%50 == 1 {
-				logger.DebugWithCtx(ctx).Msgf("%s  --> clickhouse, body(shortened): %s, ctr: %d", indexName, body.ShortString(), insertCounter.Load())
-			}
-			err := action()
-			if err != nil {
-				logger.ErrorWithCtx(ctx).Msg("Can't write to Clickhouse: " + err.Error())
-			}
-			return err
-		}
-	}
-}
-
-var matchCounter = atomic.Int32{}
-
-func findMatchingConfig(indexPattern string, cfg *QuesmaConfiguration) (IndexConfiguration, bool) {
-	matchCounter.Add(1)
-	for _, indexConfig := range cfg.IndexConfig {
-		if matchCounter.Load()%100 == 1 {
-			logger.Debug().Msgf("matching index %s with config: %+v, ctr: %d", indexPattern, indexConfig.Name, matchCounter.Load())
-		}
-		if MatchName(indexPattern, indexConfig.Name) {
-			if matchCounter.Load()%100 == 1 {
-				logger.Debug().Msgf("  ╚═ matched index %s with config: %+v, ctr: %d", indexPattern, indexConfig.Name, matchCounter.Load())
-			}
-			return indexConfig, true
-		} else {
-			if matchCounter.Load()%100 == 1 {
-				logger.Debug().Msgf("  ╚═ not matched index %s with config: %+v, ctr: %d", indexPattern, indexConfig.Name, matchCounter.Load())
-			}
-		}
-	}
-	return IndexConfiguration{}, false
-}

quesma/quesma/functionality/bulk/bulk.go
@@ -261,74 +261,71 @@ func sendToClickhouse(ctx context.Context, clickhouseDocumentsToInsert map[strin
 	for indexName, documents := range clickhouseDocumentsToInsert {
 		phoneHomeAgent.IngestCounters().Add(indexName, int64(len(documents)))
 
-		config.RunConfiguredIngest(ctx, cfg, indexName, make(types.JSON), func() error {
-			for _, document := range documents {
-				stats.GlobalStatistics.Process(cfg, indexName, document.document, clickhouse.NestedSeparator)
-			}
-			// if the index is mapped to specified database table in the configuration, use that table
-			if len(cfg.IndexConfig[indexName].Override) > 0 {
-				indexName = cfg.IndexConfig[indexName].Override
-			}
+		for _, document := range documents {
+			stats.GlobalStatistics.Process(cfg, indexName, document.document, clickhouse.NestedSeparator)
+		}
+		// if the index is mapped to specified database table in the configuration, use that table
+		if len(cfg.IndexConfig[indexName].Override) > 0 {
+			indexName = cfg.IndexConfig[indexName].Override
+		}
 
-			inserts := make([]types.JSON, len(documents))
-			for i, document := range documents {
-				inserts[i] = document.document
+		inserts := make([]types.JSON, len(documents))
+		for i, document := range documents {
+			inserts[i] = document.document
+		}
+
+		nameFormatter := clickhouse.DefaultColumnNameFormatter()
+		transformer := jsonprocessor.IngestTransformerFor(indexName, cfg)
+		err := ip.ProcessInsertQuery(ctx, indexName, inserts, transformer, nameFormatter)
+
+		for _, document := range documents {
+			bulkSingleResponse := BulkSingleResponse{
+				ID:          "fakeId",
+				Index:       document.index,
+				PrimaryTerm: 1,
+				SeqNo:       0,
+				Shards: BulkShardsResponse{
+					Failed:     0,
+					Successful: 1,
+					Total:      1,
+				},
+				Version: 0,
+				Result:  "created",
+				Status:  201,
+				Type:    "_doc",
 			}
-
-			nameFormatter := clickhouse.DefaultColumnNameFormatter()
-			transformer := jsonprocessor.IngestTransformerFor(indexName, cfg)
-			err := ip.ProcessInsertQuery(ctx, indexName, inserts, transformer, nameFormatter)
-
-			for _, document := range documents {
-				bulkSingleResponse := BulkSingleResponse{
-					ID:          "fakeId",
-					Index:       document.index,
-					PrimaryTerm: 1,
-					SeqNo:       0,
-					Shards: BulkShardsResponse{
-						Failed:     0,
-						Successful: 1,
-						Total:      1,
-					},
-					Version: 0,
-					Result:  "created",
-					Status:  201,
-					Type:    "_doc",
+			if err != nil {
+				bulkSingleResponse.Result = ""
+				bulkSingleResponse.Status = 400
+				bulkSingleResponse.Shards = BulkShardsResponse{
+					Failed:     1,
+					Successful: 0,
+					Total:      1,
 				}
-				if err != nil {
-					bulkSingleResponse.Result = ""
-					bulkSingleResponse.Status = 400
-					bulkSingleResponse.Shards = BulkShardsResponse{
-						Failed:     1,
-						Successful: 0,
-						Total:      1,
-					}
-					bulkSingleResponse.Error = queryparser.Error{
-						RootCause: []queryparser.RootCause{
-							{
-								Type:   "quesma_error",
-								Reason: err.Error(),
-							},
+				bulkSingleResponse.Error = queryparser.Error{
+					RootCause: []queryparser.RootCause{
+						{
+							Type:   "quesma_error",
+							Reason: err.Error(),
 						},
-						Type:   "quesma_error",
-						Reason: err.Error(),
-					}
-				}
-
-				// Fill out the response pointer (a pointer to the results array we will return for a bulk)
-				switch document.operation {
-				case "create":
-					document.response.Create = bulkSingleResponse
-
-				case "index":
-					document.response.Index = bulkSingleResponse
-
-				default:
-					return fmt.Errorf("unsupported bulk operation type: %s. Document: %v", document.operation, document.document)
+					},
+					Type:   "quesma_error",
+					Reason: err.Error(),
 				}
 			}
-			return nil
-		})
+
+			// Fill out the response pointer (a pointer to the results array we will return for a bulk)
+			switch document.operation {
+			case "create":
+				document.response.Create = bulkSingleResponse
+
+			case "index":
+				document.response.Index = bulkSingleResponse
+
+			default:
+				logger.Error().Msgf("unsupported bulk operation type: %s. Document: %v", document.operation, document.document)
+			}
+		}
 	}
 }
 

quesma/table_resolver/table_resolver.go
@@ -289,7 +289,7 @@ func NewTableResolver(quesmaConf config.QuesmaConfiguration, discovery clickhous
 				{"singleIndex", res.singleIndex(indexConf, IngestPipeline)},
 				{"commonTable", res.makeCommonTableResolver(indexConf, IngestPipeline)},
 
-				{"elasticAsDefault", makeElasticIsDefault(indexConf)},
+				{"defaultWildcard", makeDefaultWildcard(quesmaConf, IngestPipeline)},
 			},
 		},
 		recentDecisions: make(map[string]*Decision),
@@ -311,7 +311,7 @@ func NewTableResolver(quesmaConf config.QuesmaConfiguration, discovery clickhous
 				{"commonTable", res.makeCommonTableResolver(indexConf, QueryPipeline)},
 
 				// default action
-				{"elasticAsDefault", makeElasticIsDefault(indexConf)},
+				{"defaultWildcard", makeDefaultWildcard(quesmaConf, QueryPipeline)},
 			},
 		},
 		recentDecisions: make(map[string]*Decision),

quesma/table_resolver/table_resolver_test.go
@@ -55,7 +55,7 @@ func TestTableResolver(t *testing.T) {
 		},
 	}
 
-	cfg := config.QuesmaConfiguration{IndexConfig: indexConf}
+	cfg := config.QuesmaConfiguration{IndexConfig: indexConf, DefaultQueryTarget: []string{config.ElasticsearchTarget}, DefaultIngestTarget: []string{config.ElasticsearchTarget}}
 
 	tests := []struct {
 		name              string

commit b4be8908cb183a2fbcef3f23925dda48ab624bce

"*" configuration hot fixes
Two small bug fixes related to "*" configuration introduced in
b520acafb: "target: []" is now allowed for query processor indexes
and IsClosed of the decision is now properly filled in for
makeDefaultWildcard rule.

quesma/quesma/config/config_v2.go
@@ -381,8 +381,8 @@ func (c *QuesmaNewConfiguration) validateProcessor(p Processor) error {
 				return fmt.Errorf("index name '%s' in processor configuration is an index pattern, not allowed", indexName)
 			}
 			if p.Type == QuesmaV1ProcessorQuery {
-				if len(indexConfig.Target) != 1 && len(indexConfig.Target) != 2 {
-					return fmt.Errorf("configuration of index %s must have one or two targets (query processor)", indexName)
+				if len(indexConfig.Target) > 2 {
+					return fmt.Errorf("configuration of index %s must have at most two targets (query processor)", indexName)
 				}
 			} else {
 				if len(indexConfig.Target) > 2 {

quesma/table_resolver/rules.go
@@ -94,6 +94,7 @@ func makeDefaultWildcard(quesmaConf config.QuesmaConfiguration, pipeline string)
 
 		return &Decision{
 			UseConnectors: useConnectors,
+			IsClosed:      len(useConnectors) == 0,
 			Reason:        fmt.Sprintf("Using default wildcard ('%s') configuration for %s processor", config.DefaultWildcardIndexName, pipeline),
 		}
 	}