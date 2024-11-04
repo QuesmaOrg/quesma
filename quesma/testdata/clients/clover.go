4415532a01803c99e807b62e0313de99b50588c1
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-11-04 12:51:54 +0100

    `Filter(s)` 2nd approach (#927)
    So, this isn't a full solution.
    Before we only knew how to handle `filter(s)` when they're leaves (or
    almost) in the aggregation tree.
    Now we still can do that, but we also handle when they're the first
    aggregation layer (we just split them into multiple pancakes, with
    different `WHERE`)
    
    I find it likely that we won't really need anything more, as those 2
    cases exhaust both all our tests, and customer's case, so maybe it's
    hard to make a dashboard in Kibana with filter in the middle.
    
    But anyway even if we want to support that, adding most top-level filter
    to where clause seems like a special case and would still be the way to
    go, so this PR should be a step forward.

--- quesma/testdata/clients/clover.go
+++ quesma/testdata/clients/clover.go
@@ -473,7 +473,7 @@ var CloverTests = []testdata.AggregationTestCase{
 			  fromUnixTimestamp64Milli(1728635627125))`,
 		AdditionalAcceptableDifference: []string{"key_as_string"}, // timezone differences between local and github runs... There's always 2h difference between those, need to investigate. Maybe come back to .UTC() so there's no "+timezone" (e.g. +02:00)?
 	},
-	{ // [6]
+	{ // [3]
 		TestName: "bucket_script with multiple buckets_path",
 		QueryRequestJson: `
 		{
@@ -642,5 +642,438 @@ var CloverTests = []testdata.AggregationTestCase{
 			FROM __quesma_table_name
 			WHERE ("@timestamp">=fromUnixTimestamp64Milli(1721399904783) AND "@timestamp"<=
 			  fromUnixTimestamp64Milli(1730475504783))`,
+	},
+	{ // [4]
+		TestName: "todo",
+		QueryRequestJson: `
+		{
+			"_source": {
+				"excludes": []
+			},
+			"aggs": {
+				"other-filter": {
+					"aggs": {
+						"3": {
+							"terms": {
+								"field": "field",
+								"order": {
+									"_count": "desc"
+								},
+								"size": 15
+							}
+						}
+					},
+					"filters": {
+						"filters": {
+							"": {
+								"bool": {
+									"filter": [],
+									"must": [
+										{
+											"match_phrase": {
+												"a": "b"
+											}
+										},
+										{
+											"match_phrase": {
+												"c": "d"
+											}
+										}
+									],
+									"must_not": [],
+									"should": []
+								}
+							}
+						}
+					}
+				}
+			},
+		}`,
+		ExpectedResponse: `
+		{
+			"completion_time_in_millis": 1707486436398,
+			"expiration_time_in_millis": 1707486496397,
+			"is_partial": false,
+			"is_running": false,
+			"response": {
+				"_shards": {
+					"failed": 0,
+					"skipped": 0,
+					"successful": 1,
+					"total": 1
+				},
+				"aggregations": {
+					"other-filter": {
+						"buckets": {
+							"": {
+								"doc_count": 4675,
+								"3": {
+									"sum_other_doc_count":         0,
+									"doc_count_error_upper_bound": 0,
+									"buckets": [
+										{
+											"doc_count": 4674,
+											"key": "field"
+										},
+										{
+											"doc_count": 1,
+											"key": "bad-field"
+										}
+									]
+								}
+							}
+						}
+					}
+				},
+				"hits": {
+					"hits": [],
+					"max_score": null,
+					"total": {
+						"relation": "eq",
+						"value": 2200
+					}
+				},
+				"timed_out": false,
+				"took": 1
+			},
+			"start_time_in_millis": 1707486436397
+		}`,
+		ExpectedPancakeResults: []model.QueryResultRow{
+			{Cols: []model.QueryResultCol{
+				model.NewQueryResultCol("aggr__other-filter__count", int64(4675)),
+				model.NewQueryResultCol("aggr__other-filter__3__parent_count", int64(4675)),
+				model.NewQueryResultCol("aggr__other-filter__3__key_0", "field"),
+				model.NewQueryResultCol("aggr__other-filter__3__count", int64(4674)),
+			}},
+			{Cols: []model.QueryResultCol{
+				model.NewQueryResultCol("aggr__other-filter__count", int64(4675)),
+				model.NewQueryResultCol("aggr__other-filter__3__parent_count", int64(4675)),
+				model.NewQueryResultCol("aggr__other-filter__3__key_0", "bad-field"),
+				model.NewQueryResultCol("aggr__other-filter__3__count", int64(1)),
+			}},
+		},
+		ExpectedPancakeSQL: `
+			SELECT sum(count(*)) OVER () AS "aggr__other-filter__count",
+			  sum(count(*)) OVER () AS "aggr__other-filter__3__parent_count",
+			  "field" AS "aggr__other-filter__3__key_0",
+			  count(*) AS "aggr__other-filter__3__count"
+			FROM __quesma_table_name
+			WHERE ("a" iLIKE '%b%' AND "c" iLIKE '%d%')
+			GROUP BY "field" AS "aggr__other-filter__3__key_0"
+			ORDER BY "aggr__other-filter__3__count" DESC,
+			  "aggr__other-filter__3__key_0" ASC
+			LIMIT 16`,
+	},
+	{ // [5]
+		TestName: "todo",
+		QueryRequestJson: `
+		{
+			"aggs": {
+				"q": {
+					"aggs": {
+						"time_buckets": {
+							"aggs": {
+								"count": {
+									"bucket_script": {
+										"buckets_path": "_count",
+										"script": {
+											"lang": "expression",
+											"source": "_value"
+										}
+									}
+								}
+							},
+							"date_histogram": {
+								"extended_bounds": {
+									"max": 1728507732621,
+									"min": 1728507729621
+								},
+								"field": "@timestamp",
+								"fixed_interval": "7d",
+								"min_doc_count": 0
+							},
+							"meta": {
+								"type": "time_buckets"
+							}
+						}
+					},
+					"filters": {
+						"filters": {
+							"*": {
+								"query_string": {
+									"query": "*"
+								}
+							}
+						}
+					},
+					"meta": {
+						"type": "split"
+					}
+				}
+			},
+			"query": {
+				"bool": {
+					"filter": {
+						"bool": {
+							"filter": [],
+							"must": [],
+							"must_not": [],
+							"should": []
+						}
+					},
+					"must": [
+						{
+							"range": {
+								"@timestamp": {
+									"format": "strict_date_optional_time",
+									"gte": "2024-10-09T21:02:09.621Z",
+									"lte": "2024-10-09T21:02:12.621Z"
+								}
+							}
+						}
+					]
+				}
+			},
+			"runtime_mappings": {},
+			"size": 0,
+			"timeout": "30000ms",
+			"track_total_hits": true
+		}`,
+		ExpectedResponse: `
+		{
+			"completion_time_in_millis": 1707486436398,
+			"expiration_time_in_millis": 1707486496397,
+			"is_partial": false,
+			"is_running": false,
+			"response": {
+				"_shards": {
+					"failed": 0,
+					"skipped": 0,
+					"successful": 1,
+					"total": 1
+				},
+				"aggregations": {
+					"q": {
+						"meta": {
+							"type": "split"
+						},
+						"buckets": {
+							"*": {
+								"doc_count": 4675,
+								"time_buckets": {
+									"meta": {
+										"type": "time_buckets"
+									},
+									"buckets": [
+										{
+											"doc_count": 442,
+											"key": 1726704000000,
+											"key_as_string": "2024-09-19T00:00:00.000",
+											"count": {
+												"value": 442
+											}
+										},
+										{
+											"doc_count": 0,
+											"key": 1727308800000,
+											"key_as_string": "2024-09-26T00:00:00.000"
+										},
+										{
+											"doc_count": 0,
+											"key": 1727913600000,
+											"key_as_string": "2024-10-03T00:00:00.000"
+										},
+										{
+											"doc_count": 0,
+											"key": 1728518400000,
+											"key_as_string": "2024-10-10T00:00:00.000"
+										},
+										{
+											"doc_count": 1,
+											"key": 1729123200000,
+											"key_as_string": "2024-10-17T00:00:00.000",
+											"count": {
+												"value": 1.0
+											}
+										}
+									]
+								}
+							}
+						}
+					}
+				},
+				"hits": {
+					"hits": [],
+					"max_score": null,
+					"total": {
+						"relation": "eq",
+						"value": 2200
+					}
+				},
+				"timed_out": false,
+				"took": 1
+			},
+			"start_time_in_millis": 1707486436397
+		}`,
+		ExpectedPancakeResults: []model.QueryResultRow{
+			{Cols: []model.QueryResultCol{
+				model.NewQueryResultCol("aggr__q__count", int64(4675)),
+				model.NewQueryResultCol("aggr__q__time_buckets__key_0", int64(2855)),
+				model.NewQueryResultCol("aggr__q__time_buckets__count", int64(442)),
+			}},
+			{Cols: []model.QueryResultCol{
+				model.NewQueryResultCol("aggr__q__count", int64(4675)),
+				model.NewQueryResultCol("aggr__q__time_buckets__key_0", int64(2859)),
+				model.NewQueryResultCol("aggr__q__time_buckets__count", int64(1)),
+			}},
+		},
+		ExpectedPancakeSQL: `
+			SELECT sum(count(*)) OVER () AS "aggr__q__count",
+			  toInt64(toUnixTimestamp64Milli("@timestamp") / 604800000) AS
+			  "aggr__q__time_buckets__key_0", count(*) AS "aggr__q__time_buckets__count"
+			FROM __quesma_table_name
+			WHERE (("@timestamp">=fromUnixTimestamp64Milli(1728507729621) AND "@timestamp"<=
+			  fromUnixTimestamp64Milli(1728507732621)) AND "__quesma_fulltext_field_name"
+			  ILIKE '%')
+			GROUP BY toInt64(toUnixTimestamp64Milli("@timestamp") / 604800000) AS
+			  "aggr__q__time_buckets__key_0"
+			ORDER BY "aggr__q__time_buckets__key_0" ASC`,
+	},
+	{ // [6]
+		TestName: "Clover",
+		QueryRequestJson: `
+		{
+			"aggs": {
+				"q": {
+					"aggs": {
+						"time_buckets": {
+							"aggs": {
+								"sum(count)": {
+									"sum": {
+										"field": "count"
+									}
+								}
+							},
+							"date_histogram": {
+								"field": "@timestamp",
+								"fixed_interval": "30m",
+								"min_doc_count": 0,
+								"time_zone": "Europe/Warsaw"
+							},
+							"meta": {
+								"type": "time_buckets"
+							}
+						}
+					},
+					"filters": {
+						"filters": {
+							"!str_field:CRASH": {
+								"query_string": {
+									"query": "!str_field:CRASH"
+								}
+							}
+						}
+					},
+					"meta": {
+						"type": "split"
+					}
+				}
+			},
+			"runtime_mappings": {},
+			"size": 0,
+			"timeout": "30000ms",
+			"track_total_hits": true
+		}`,
+		ExpectedResponse: `
+		{
+			"completion_time_in_millis": 1707486436398,
+			"expiration_time_in_millis": 1707486496397,
+			"is_partial": false,
+			"is_running": false,
+			"response": {
+				"_shards": {
+					"failed": 0,
+					"skipped": 0,
+					"successful": 1,
+					"total": 1
+				},
+				"aggregations": {
+					"sampler": {
+						"doc_count": 4675,
+						"eventRate": {
+							"buckets": [
+								{
+									"doc_count": 442,
+									"key": 1726351200000,
+									"key_as_string": "2024-09-14T22:00:00.000"
+								},
+								{
+									"doc_count": 0,
+									"key": 1726353000000,
+									"key_as_string": "2024-09-14T22:30:00.000"
+								},
+								{
+									"doc_count": 0,
+									"key": 1726354800000,
+									"key_as_string": "2024-09-14T23:00:00.000"
+								},
+								{
+									"doc_count": 0,
+									"key": 1726356600000,
+									"key_as_string": "2024-09-14T23:30:00.000"
+								},
+								{
+									"doc_count": 0,
+									"key": 1726356600000,
+									"key_as_string": "2024-09-15T00:00:00.000"
+								},
+								{
+									"doc_count": 1,
+									"key": 1728777600000,
+									"key_as_string": "2024-09-15T00:30:00.000"
+								}
+							]
+						}
+					}
+				},
+				"hits": {
+					"hits": [],
+					"max_score": null,
+					"total": {
+						"relation": "eq",
+						"value": 2200
+					}
+				},
+				"timed_out": false,
+				"took": 1
+			},
+			"start_time_in_millis": 1707486436397
+		}`,
+		ExpectedPancakeResults: []model.QueryResultRow{
+			{Cols: []model.QueryResultCol{
+				model.NewQueryResultCol("aggr__q__count", int64(4675)),
+				model.NewQueryResultCol("aggr__q__time_buckets__key_0", int64(1726358400000/1800000)),
+				model.NewQueryResultCol("aggr__q__time_buckets__count", int64(442)),
+				model.NewQueryResultCol("metric__q__time_buckets__sum(count)_col_0", int64(442)),
+			}},
+			{Cols: []model.QueryResultCol{
+				model.NewQueryResultCol("aggr__q__count", int64(4675)),
+				model.NewQueryResultCol("aggr__q__time_buckets__key_0", int64(1726367400000/1800000)),
+				model.NewQueryResultCol("aggr__q__time_buckets__count", int64(1)),
+				model.NewQueryResultCol("metric__q__time_buckets__sum(count)_col_0", int64(1)),
+			}},
+		},
+		ExpectedPancakeSQL: `
+			SELECT sum(count(*)) OVER () AS "aggr__q__count",
+			  toInt64((toUnixTimestamp64Milli("@timestamp")+timeZoneOffset(toTimezone(
+			  "@timestamp", 'Europe/Warsaw'))*1000) / 1800000) AS
+			  "aggr__q__time_buckets__key_0", count(*) AS "aggr__q__time_buckets__count",
+			  sumOrNull("count") AS "metric__q__time_buckets__sum(count)_col_0"
+			FROM __quesma_table_name
+			WHERE NOT ("str_field" = 'CRASH')
+			GROUP BY toInt64((toUnixTimestamp64Milli("@timestamp")+timeZoneOffset(toTimezone
+			  ("@timestamp", 'Europe/Warsaw'))*1000) / 1800000) AS
+			  "aggr__q__time_buckets__key_0"
+			ORDER BY "aggr__q__time_buckets__key_0" ASC`,
 	},
 }
