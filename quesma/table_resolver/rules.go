69b408a4e6aafe790828294672a282e423a5ca83
Piotr Grabowski <piotr@quesma.com>
2024-11-05 06:35:55 +0100

    cleanups

--- quesma/table_resolver/rules.go
+++ quesma/table_resolver/rules.go
@@ -10,7 +10,6 @@ import (
 	"quesma/quesma/config"
 	"quesma/util"
 	"reflect"
-	"slices"
 	"strings"
 )
 
@@ -76,18 +75,15 @@ func singleIndexSplitter(pattern string) (parsedPattern, *Decision) {
 	}, nil
 }
 
-func makeIsDisabledInConfig(cfg map[string]config.IndexConfiguration, pipeline string) func(input parsedPattern) *Decision {
+func makeIsDisabledInConfig(cfg map[string]config.IndexConfiguration, pipeline string) func(part string) *Decision {
 
-	return func(input parsedPattern) *Decision {
-
-		if !input.isPattern {
-			idx, ok := cfg[input.source]
-			if ok {
-				if len(getTargets(idx, pipeline)) == 0 {
-					return &Decision{
-						IsClosed: true,
-						Reason:   "Index is disabled in config.",
-					}
+	return func(part string) *Decision {
+		idx, ok := cfg[part]
+		if ok {
+			if len(getTargets(idx, pipeline)) == 0 {
+				return &Decision{
+					IsClosed: true,
+					Reason:   "Index is disabled in config.",
 				}
 			}
 		}
@@ -96,9 +92,9 @@ func makeIsDisabledInConfig(cfg map[string]config.IndexConfiguration, pipeline s
 	}
 }
 
-func resolveInternalElasticName(pattern parsedPattern) *Decision {
+func resolveInternalElasticName(part string) *Decision {
 
-	if elasticsearch.IsInternalIndex(pattern.source) {
+	if elasticsearch.IsInternalIndex(part) {
 		return &Decision{
 			UseConnectors: []ConnectorDecision{&ConnectorDecisionElastic{ManagementCall: true}},
 			Reason:        "It's kibana internals",
@@ -108,8 +104,8 @@ func resolveInternalElasticName(pattern parsedPattern) *Decision {
 	return nil
 }
 
-func makeDefaultWildcard(quesmaConf config.QuesmaConfiguration, pipeline string) func(input parsedPattern) *Decision {
-	return func(input parsedPattern) *Decision {
+func makeDefaultWildcard(quesmaConf config.QuesmaConfiguration, pipeline string) func(part string) *Decision {
+	return func(part string) *Decision {
 		var targets []string
 		var useConnectors []ConnectorDecision
 
@@ -129,9 +125,9 @@ func makeDefaultWildcard(quesmaConf config.QuesmaConfiguration, pipeline string)
 			switch target {
 			case config.ClickhouseTarget:
 				useConnectors = append(useConnectors, &ConnectorDecisionClickhouse{
-					ClickhouseTableName: input.source,
+					ClickhouseTableName: part,
 					IsCommonTable:       quesmaConf.UseCommonTableForWildcard,
-					ClickhouseTables:    []string{input.source},
+					ClickhouseTables:    []string{part},
 				})
 			case config.ElasticsearchTarget:
 				useConnectors = append(useConnectors, &ConnectorDecisionElastic{})
@@ -151,15 +147,10 @@ func makeDefaultWildcard(quesmaConf config.QuesmaConfiguration, pipeline string)
 	}
 }
 
-func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfiguration, pipeline string) func(input parsedPattern) *Decision {
+func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfiguration, pipeline string) func(part string) *Decision {
 
-	return func(input parsedPattern) *Decision {
-
-		if input.isPattern {
-			return nil
-		}
-
-		if cfg, ok := indexConfig[input.source]; ok {
+	return func(part string) *Decision {
+		if cfg, ok := indexConfig[part]; ok {
 			if !cfg.UseCommonTable {
 
 				targets := getTargets(cfg, pipeline)
@@ -182,8 +173,8 @@ func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfi
 						targetDecision = &ConnectorDecisionElastic{}
 					case config.ClickhouseTarget:
 						targetDecision = &ConnectorDecisionClickhouse{
-							ClickhouseTableName: input.source,
-							ClickhouseTables:    []string{input.source},
+							ClickhouseTableName: part,
+							ClickhouseTables:    []string{part},
 						}
 					default:
 						return &Decision{
@@ -203,8 +194,8 @@ func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfi
 							Reason: "Enabled in the config. Dual write is enabled.",
 
 							UseConnectors: []ConnectorDecision{&ConnectorDecisionClickhouse{
-								ClickhouseTableName: input.source,
-								ClickhouseTables:    []string{input.source}},
+								ClickhouseTableName: part,
+								ClickhouseTables:    []string{part}},
 								&ConnectorDecisionElastic{}},
 						}
 
@@ -216,8 +207,8 @@ func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfi
 								Reason:          "Enabled in the config. A/B testing.",
 								EnableABTesting: true,
 								UseConnectors: []ConnectorDecision{&ConnectorDecisionClickhouse{
-									ClickhouseTableName: input.source,
-									ClickhouseTables:    []string{input.source}},
+									ClickhouseTableName: part,
+									ClickhouseTables:    []string{part}},
 									&ConnectorDecisionElastic{}},
 							}
 						} else if targets[0] == config.ElasticsearchTarget && targets[1] == config.ClickhouseTarget {
@@ -228,8 +219,8 @@ func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfi
 								UseConnectors: []ConnectorDecision{
 									&ConnectorDecisionElastic{},
 									&ConnectorDecisionClickhouse{
-										ClickhouseTableName: input.source,
-										ClickhouseTables:    []string{input.source}},
+										ClickhouseTableName: part,
+										ClickhouseTables:    []string{part}},
 								},
 							}
 
@@ -260,186 +251,10 @@ func (r *tableRegistryImpl) singleIndex(indexConfig map[string]config.IndexConfi
 	}
 }
 
-func (r *tableRegistryImpl) makeCheckIfPatternMatchesAllConnectors(pipeline string) func(input parsedPattern) *Decision {
+func (r *tableRegistryImpl) makeCommonTableResolver(cfg map[string]config.IndexConfiguration, pipeline string) func(part string) *Decision {
 
-	return func(input parsedPattern) *Decision {
-		if input.isPattern {
-
-			var matchedElastic []string
-			var matchedClickhouse []string
-
-			for _, pattern := range input.parts {
-
-				// here we check against the config
-
-				for indexName, index := range r.conf.IndexConfig {
-					targets := getTargets(index, pipeline)
-
-					if util.IndexPatternMatches(pattern, indexName) {
-
-						for _, target := range targets {
-							switch target {
-							case config.ElasticsearchTarget:
-								matchedElastic = append(matchedElastic, indexName)
-							case config.ClickhouseTarget:
-								matchedClickhouse = append(matchedClickhouse, indexName)
-							default:
-								return &Decision{
-									Err:    end_user_errors.ErrSearchCondition.New(fmt.Errorf("unsupported target: %s", target)),
-									Reason: "Unsupported target.",
-								}
-							}
-						}
-					}
-				}
-
-				// but maybe we should also check against the actual indexes ??
-				for indexName := range r.elasticIndexes {
-					if util.IndexPatternMatches(pattern, indexName) {
-						matchedElastic = append(matchedElastic, indexName)
-					}
-				}
-				if r.conf.AutodiscoveryEnabled {
-					for tableName := range r.clickhouseIndexes {
-						if util.IndexPatternMatches(pattern, tableName) {
-							matchedClickhouse = append(matchedClickhouse, tableName)
-						}
-					}
-				}
-
-			}
-
-			matchedElastic = util.Distinct(matchedElastic)
-			matchedClickhouse = util.Distinct(matchedClickhouse)
-
-			nElastic := len(matchedElastic)
-			nClickhouse := len(matchedClickhouse)
-
-			switch {
-
-			case nElastic > 0 && nClickhouse > 0:
-				return &Decision{
-					Err:    end_user_errors.ErrSearchCondition.New(fmt.Errorf("index pattern [%s] resolved to both elasticsearch indices: [%s] and clickhouse tables: [%s]", input.parts, matchedElastic, matchedClickhouse)),
-					Reason: "Both Elastic and Clickhouse matched.",
-				}
-
-			case nElastic > 0 && nClickhouse == 0:
-
-				return &Decision{
-					UseConnectors: []ConnectorDecision{&ConnectorDecisionElastic{}},
-					Reason:        "Only Elastic matched.",
-				}
-
-			case nElastic == 0 && nClickhouse > 0:
-				// it will be resolved by sth else later
-				return nil
-
-			case nElastic == 0 && nClickhouse == 0:
-				return &Decision{
-					IsEmpty: true,
-					Reason:  "No indexes matched. Checked both connectors.",
-				}
-			}
-		}
-
-		return nil
-	}
-
-}
-
-func (r *tableRegistryImpl) makeCommonTableResolver(cfg map[string]config.IndexConfiguration, pipeline string) func(input parsedPattern) *Decision {
-
-	return func(input parsedPattern) *Decision {
-
-		if input.isPattern {
-
-			// At this point we should do not have any elastic indexes.
-			// This is because we have already checked if the pattern matches any elastic indexes.
-			for _, pattern := range input.parts {
-				for indexName := range r.elasticIndexes {
-					if util.IndexPatternMatches(pattern, indexName) {
-						return &Decision{
-							Err:    end_user_errors.ErrSearchCondition.New(fmt.Errorf("index parsedPattern [%s] resolved to elasticsearch indices", input.parts)),
-							Reason: "We're not supporting common tables for Elastic.",
-						}
-					}
-				}
-			}
-
-			var matchedVirtualTables []string
-			var matchedTables []string
-			for _, pattern := range input.parts {
-
-				// here we check against the config
-
-				for indexName, index := range r.conf.IndexConfig {
-					if util.IndexPatternMatches(pattern, indexName) {
-
-						targets := getTargets(index, pipeline)
-
-						if slices.Contains(targets, config.ClickhouseTarget) {
-							if index.UseCommonTable {
-								matchedVirtualTables = append(matchedVirtualTables, indexName)
-							} else {
-								matchedTables = append(matchedTables, indexName)
-							}
-						}
-					}
-				}
-
-				// but maybe we should also check against the actual indexes ??
-				if r.conf.AutodiscoveryEnabled {
-					for indexName, index := range r.clickhouseIndexes {
-						if util.IndexPatternMatches(pattern, indexName) {
-							if index.isVirtual {
-								matchedVirtualTables = append(matchedVirtualTables, indexName)
-							} else {
-								matchedTables = append(matchedTables, indexName)
-							}
-						}
-					}
-				}
-			}
-
-			matchedTables = util.Distinct(matchedTables)
-			matchedVirtualTables = util.Distinct(matchedVirtualTables)
-
-			switch {
-
-			case len(matchedTables) == 0 && len(matchedVirtualTables) == 0:
-				return &Decision{
-					IsEmpty: true,
-					Reason:  "No indexes found.",
-				}
-
-			case len(matchedTables) == 1 && len(matchedVirtualTables) == 0:
-				return &Decision{
-					UseConnectors: []ConnectorDecision{&ConnectorDecisionClickhouse{
-						ClickhouseTableName: matchedTables[0],
-						ClickhouseTables:    []string{matchedTables[0]},
-					}},
-					Reason: "Pattern matches single standalone table.",
-				}
-
-			case len(matchedTables) == 0 && len(matchedVirtualTables) > 0:
-				return &Decision{
-					UseConnectors: []ConnectorDecision{&ConnectorDecisionClickhouse{
-						IsCommonTable:       true,
-						ClickhouseTableName: common_table.TableName,
-						ClickhouseTables:    matchedVirtualTables,
-					}},
-					Reason: "Common table will be used. Querying multiple indexes.",
-				}
-
-			default:
-				return &Decision{
-					Err:    end_user_errors.ErrSearchCondition.New(fmt.Errorf("index pattern [%s] resolved to both standalone table indices: [%s] and common table indices: [%s]", input.source, matchedTables, matchedVirtualTables)),
-					Reason: "Both standalone table and common table indexes matches the pattern",
-				}
-			}
-		}
-
-		if input.source == common_table.TableName {
+	return func(part string) *Decision {
+		if part == common_table.TableName {
 			return &Decision{
 				Err:    fmt.Errorf("common table is not allowed to be queried directly"),
 				Reason: "It's internal table. Not allowed to be queried directly.",
@@ -450,18 +265,18 @@ func (r *tableRegistryImpl) makeCommonTableResolver(cfg map[string]config.IndexC
 
 		if r.conf.AutodiscoveryEnabled {
 			for indexName, index := range r.clickhouseIndexes {
-				if index.isVirtual && indexName == input.source {
+				if index.isVirtual && indexName == part {
 					virtualTableExists = true
 					break
 				}
 			}
 		}
 
-		if idxConfig, ok := cfg[input.source]; (ok && idxConfig.UseCommonTable) || (virtualTableExists) {
+		if idxConfig, ok := cfg[part]; (ok && idxConfig.UseCommonTable) || (virtualTableExists) {
 			return &Decision{
 				UseConnectors: []ConnectorDecision{&ConnectorDecisionClickhouse{
 					ClickhouseTableName: common_table.TableName,
-					ClickhouseTables:    []string{input.source},
+					ClickhouseTables:    []string{part},
 					IsCommonTable:       true,
 				}},
 				Reason: "Common table will be used.",
