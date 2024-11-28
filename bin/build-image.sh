70e5ca39f548c249e8356cc2239edec254b0a677
Rafal Strzalinski <rafal@quesma.com>
2024-11-28 15:10:48 +0100

    Add passing build info

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -2,4 +2,9 @@
 # Rebuilds the Quesma Docker image and tags it as quesma:nightly
 set -e
 cd "$(dirname "$0/")/.."
-docker build -f quesma/Dockerfile -t quesma/quesma:nightly quesma
+
+QUESMA_BUILD_SHA=$(git rev-parse HEAD)
+QUESMA_BUILD_DATE=$(date -u +"%Y-%m-%d %H:%M:%S")
+QUESMA_VERSION="development"
+
+docker build --build-arg QUESMA_BUILD_DATE="$QUESMA_BUILD_DATE" --build-arg QUESMA_VERSION="$QUESMA_VERSION" --build-arg QUESMA_BUILD_SHA="$QUESMA_BUILD_SHA" -f quesma/Dockerfile -t quesma/quesma:nightly quesma
