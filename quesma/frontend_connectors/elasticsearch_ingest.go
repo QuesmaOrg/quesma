885ea152aa52d5e17ecb1509d6d7366d5b9f5a67
Przemysław Hejman <przemyslaw.hejman@quesma.com>
2024-12-06 12:56:26 +0100

    Adding very naive (dumb) ingest using v2 api (#1055)
    Very rough implementation of the ingest endpoints with `v2` api, but I
    think we should merge this and continue working on such "sketchpad". It
    basically shows the way.
    
    ---------
    
    Signed-off-by: Przemysław Hejman <przemyslaw.hejman@quesma.com>

---
+++ quesma/frontend_connectors/elasticsearch_ingest.go
@@ -0,0 +1,101 @@
+// Copyright Quesma, licensed under the Elastic License 2.0.
+// SPDX-License-Identifier: Elastic-2.0
+
+package frontend_connectors
+
+import (
+	"fmt"
+	"github.com/ucarion/urlpath"
+	"net/http"
+	quesma_api "quesma_v2/core"
+)
+
+type ElasticsearchIngestFrontendConnector struct {
+	BasicHTTPFrontendConnector
+}
+
+const (
+	IndexDocPath  = "/:index/_doc"
+	IndexBulkPath = "/:index/_bulk"
+
+	// IngestAction and below are metadata items passed to processor.
+	IngestAction    = "ingest_action"
+	DocIndexAction  = "_doc"
+	BulkIndexAction = "_bulk"
+	IngestTargetKey = "ingest_target"
+	// TODO: this actually should not be a dependency on processor
+)
+
+func NewElasticsearchIngestFrontendConnector(endpoint string) *ElasticsearchIngestFrontendConnector {
+	fc := &ElasticsearchIngestFrontendConnector{
+		BasicHTTPFrontendConnector: BasicHTTPFrontendConnector{
+			endpoint: endpoint,
+		},
+	}
+	router := NewHTTPRouter()
+	router.AddRoute(IndexBulkPath, bulk)
+	router.AddRoute(IndexDocPath, doc)
+	fc.AddRouter(router)
+	return fc
+}
+
+func getMatchingHandler(requestPath string, handlers map[string]quesma_api.HandlersPipe) *quesma_api.HandlersPipe {
+	for path, handler := range handlers {
+		urlPath := urlpath.New(path)
+		_, matches := urlPath.Match(requestPath)
+		if matches {
+			return &handler
+		}
+	}
+	return nil
+}
+
+func (h *ElasticsearchIngestFrontendConnector) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	handlers := h.router.GetHandlers()
+	handlerWrapper := getMatchingHandler(req.URL.Path, handlers)
+	if handlerWrapper == nil {
+		h.router.Multiplexer().ServeHTTP(w, req)
+		return
+	}
+	dispatcher := &quesma_api.Dispatcher{}
+
+	// for the response out we are Elasticsearch-7 compliant
+	w.Header().Set("Content-Type", "application/json")
+	http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		metadata, message, _ := handlerWrapper.Handler(req)
+		req.Header.Set("x-przemek", "blah")
+		_, message = dispatcher.Dispatch(handlerWrapper.Processors, metadata, message)
+		_, err := w.Write(message.([]byte))
+		if err != nil {
+			fmt.Printf("Error writing response: %s\n", err)
+		}
+	}).ServeHTTP(w, req)
+}
+
+func bulk(request *http.Request) (map[string]interface{}, any, error) {
+	//body, err := ReadRequestBody(request)
+	//if err != nil {
+	//	return nil, nil, err
+	//}
+	metadata := quesma_api.MakeNewMetadata()
+	metadata[IngestAction] = BulkIndexAction
+	metadata[IngestTargetKey] = getIndexFromRequest(request)
+	return metadata, request, nil
+}
+
+func doc(request *http.Request) (map[string]interface{}, any, error) {
+	//body, err := ReadRequestBody(request)
+	//if err != nil {
+	//	return nil, nil, err
+	//}
+	metadata := quesma_api.MakeNewMetadata()
+	metadata[IngestAction] = DocIndexAction
+	metadata[IngestTargetKey] = getIndexFromRequest(request)
+	return metadata, request, nil
+}
+
+func getIndexFromRequest(request *http.Request) string {
+	expectedUrl := urlpath.New("/:index/*")
+	match, _ := expectedUrl.Match(request.URL.Path) // safe to call at this level
+	return match.Params["index"]
+}
---
+++ quesma/processors/es_to_ch_ingest/elasticsearch_to_clickhouse_ingest_processor.go
@@ -0,0 +1,164 @@
+// Copyright Quesma, licensed under the Elastic License 2.0.
+// SPDX-License-Identifier: Elastic-2.0
+
+package es_to_ch_ingest
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/goccy/go-json"
+	"github.com/rs/zerolog/log"
+	"io"
+	"net/http"
+	"net/url"
+	"quesma/backend_connectors"
+	"quesma/clickhouse"
+	"quesma/common_table"
+	"quesma/frontend_connectors"
+	"quesma/ingest"
+	"quesma/persistence"
+	"quesma/processors"
+	"quesma/quesma/config"
+	"quesma/quesma/types"
+	"quesma/schema"
+	"quesma_v2/core"
+)
+
+const (
+	IngestAction    = "ingest_action"
+	DocIndexAction  = "_doc"
+	BulkIndexAction = "_bulk"
+	IngestTargetKey = "ingest_target"
+)
+
+type ElasticsearchToClickHouseIngestProcessor struct {
+	processors.BaseProcessor
+	config config.QuesmaProcessorConfig
+}
+
+func NewElasticsearchToClickHouseIngestProcessor(conf config.QuesmaProcessorConfig) *ElasticsearchToClickHouseIngestProcessor {
+	return &ElasticsearchToClickHouseIngestProcessor{
+		BaseProcessor: processors.NewBaseProcessor(),
+		config:        conf,
+	}
+}
+
+func (p *ElasticsearchToClickHouseIngestProcessor) GetId() string {
+	return "elasticsearch_to_clickhouse_ingest"
+}
+
+// prepareTemporaryIngestProcessor creates a temporary ingest processor which is a new version of the ingest processor,
+// which uses `quesma_api.BackendConnector` instead of `*sql.DB` for the database connection.
+func (p *ElasticsearchToClickHouseIngestProcessor) prepareTemporaryIngestProcessor(connector quesma_api.BackendConnector) *ingest.IngestProcessor2 {
+	u, _ := url.Parse("http://localhost:9200")
+
+	elasticsearchConfig := config.ElasticsearchConfiguration{
+		Url: (*config.Url)(u),
+	}
+	oldQuesmaConfig := &config.QuesmaConfiguration{
+		IndexConfig: p.config.IndexConfig,
+	}
+
+	virtualTableStorage := persistence.NewElasticJSONDatabase(elasticsearchConfig, common_table.VirtualTableElasticIndexName)
+	tableDisco := clickhouse.NewTableDiscovery2(oldQuesmaConfig, connector, virtualTableStorage)
+	schemaRegistry := schema.NewSchemaRegistry(clickhouse.TableDiscoveryTableProviderAdapter{TableDiscovery: tableDisco}, oldQuesmaConfig, clickhouse.SchemaTypeAdapter{})
+
+	v2TableResolver := NewNextGenTableResolver()
+
+	ip := ingest.NewIngestProcessor2(oldQuesmaConfig, connector, nil, tableDisco, schemaRegistry, virtualTableStorage, v2TableResolver)
+	ip.Start()
+	return ip
+}
+
+func (p *ElasticsearchToClickHouseIngestProcessor) Handle(metadata map[string]interface{}, message ...any) (map[string]interface{}, any, error) {
+	var data []byte
+	var chBackend, esBackend quesma_api.BackendConnector
+	indexNameFromIncomingReq := metadata[IngestTargetKey].(string)
+	if indexNameFromIncomingReq == "" {
+		panic("NO INDEX NAME?!?!?")
+	}
+
+	if chBackend = p.GetBackendConnector(quesma_api.ClickHouseSQLBackend); chBackend == nil {
+		fmt.Println("Backend connector not found")
+		return metadata, data, nil
+	}
+
+	tempIngestProcessor := p.prepareTemporaryIngestProcessor(chBackend)
+
+	esBackend = p.GetBackendConnector(quesma_api.ElasticsearchBackend)
+	if esBackend == nil {
+		fmt.Println("Backend connector not found")
+		return metadata, data, nil
+	}
+	es, ok := esBackend.(*backend_connectors.ElasticsearchBackendConnector) // OKAY JUST FOR NOW
+	if !ok {
+		panic(" !!! ")
+	}
+
+	for _, m := range message {
+		messageAsHttpReq, err := quesma_api.CheckedCast[*http.Request](m)
+		if err != nil {
+			panic("ElasticsearchToClickHouseIngestProcessor: invalid message type")
+		}
+
+		if _, present := p.config.IndexConfig[indexNameFromIncomingReq]; !present {
+			// route to Elasticsearch
+			resp := es.Send(messageAsHttpReq)
+			respBody, err := ReadResponseBody(resp)
+			if err != nil {
+				println(err)
+			}
+			return metadata, respBody, nil
+		}
+
+		bodyAsBytes, err := frontend_connectors.ReadRequestBody(messageAsHttpReq)
+		if err != nil {
+			panic("ElasticsearchToClickHouseIngestProcessor: invalid message type")
+		}
+
+		switch metadata[IngestAction] {
+		case DocIndexAction:
+			payloadJson, err := types.ExpectJSON(types.ParseRequestBody(string(bodyAsBytes)))
+			if err != nil {
+				println(err)
+			}
+			result, err := handleDocIndex(payloadJson, indexNameFromIncomingReq, tempIngestProcessor, p.config)
+			if err != nil {
+				println(err)
+			}
+			if respBody, err := json.Marshal(result.Index); err == nil {
+				return metadata, respBody, nil
+			}
+		case BulkIndexAction:
+			payloadNDJson, err := types.ExpectNDJSON(types.ParseRequestBody(string(bodyAsBytes)))
+			if err != nil {
+				println(err)
+			}
+			results, err := handleBulkIndex(payloadNDJson, indexNameFromIncomingReq, tempIngestProcessor, es, p.config)
+			if err != nil {
+				println(err)
+			}
+			if respBody, err := json.Marshal(results); err == nil {
+				return metadata, respBody, nil
+			}
+			println("BulkIndexAction")
+		default:
+			log.Info().Msg("Rethink you whole life and start over again")
+		}
+
+	}
+	return metadata, data, nil
+}
+
+func (p *ElasticsearchToClickHouseIngestProcessor) GetSupportedBackendConnectors() []quesma_api.BackendConnectorType {
+	return []quesma_api.BackendConnectorType{quesma_api.ClickHouseSQLBackend, quesma_api.ElasticsearchBackend}
+}
+
+func ReadResponseBody(resp *http.Response) ([]byte, error) {
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body = io.NopCloser(bytes.NewBuffer(respBody))
+	return respBody, nil
+}
