c0dbb51c296be6169293f459cbc7c24191230c72
Jacek Migdal <jacek@quesma.com>
2024-12-09 14:37:31 +0100

    Works on dev version

--- quesma/quesma/functionality/field_capabilities/field_caps.go
+++ quesma/quesma/functionality/field_capabilities/field_caps.go
@@ -101,7 +101,7 @@ func EmptyFieldCapsResponse() []byte {
 }
 
 func HandleFieldCaps(ctx context.Context, cfg *config.QuesmaConfiguration, schemaRegistry schema.Registry, index string, lm *clickhouse.LogManager) ([]byte, error) {
-	indexes, err := lm.ResolveIndexPattern(ctx, index)
+	indexes, err := lm.ResolveIndexPatternV2(schemaRegistry, ctx, index)
 	if err != nil {
 		return nil, err
 	}
