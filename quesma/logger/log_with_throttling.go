0e4fddd1311e2232e53b5ac40f3645db4774548e
Krzysztof Kiewicz <krzysztof@quesma.com>
2024-12-13 20:27:32 +0100

    Increase throttle duration 3->30min

--- quesma/logger/log_with_throttling.go
+++ quesma/logger/log_with_throttling.go
@@ -10,12 +10,16 @@ import (
 
 var throttleMap = util.SyncMap[string, time.Time]{}
 
+const throttleDuration = 30 * time.Minute
+
 // WarnWithCtxAndThrottling - logs a warning message when encountering unexpected parameter in query.
-// Very simple mechanism, probably will require (slight) refactor, if you need it for other use cases.
+// We only log once per throttleDuration for each aggrName+paramName combination, so that we don't spam the logs.
+// Very simple mechanism, good enough for this specific use case.
+// Probably will require (at least slight) refactor, if you need it for some other things.
 func WarnWithCtxAndThrottling(ctx context.Context, aggrName, paramName, format string, v ...any) {
 	mapKey := aggrName + paramName
 	timestamp, ok := throttleMap.Load(mapKey)
-	weThrottle := ok && time.Since(timestamp) < 3*time.Minute
+	weThrottle := ok && time.Since(timestamp) < throttleDuration
 	if !weThrottle {
 		WarnWithCtx(ctx).Msgf(format, v...)
 		throttleMap.Store(mapKey, time.Now())
