421ed98ae38b64c7aa472b295233770146fdc65d
przemyslaw <przemyslaw.hejman@quesma.com>
2025-01-02 12:32:34 +0100

    quesma_v2 = true

--- quesma/main.go
+++ quesma/main.go
@@ -209,7 +209,8 @@ func constructQuesma(cfg *config.QuesmaConfiguration, sl clickhouse.TableDiscove
 	if cfg.TransparentProxy {
 		return quesma.NewQuesmaTcpProxy(cfg, quesmaManagementConsole, logChan, false)
 	} else {
-		const quesma_v2 = false
+		//const quesma_v2 = false TODO: bring this back
+		const quesma_v2 = true
 		return quesma.NewHttpProxy(phoneHomeAgent, lm, ip, sl, im, schemaRegistry, cfg, quesmaManagementConsole, abResultsrepository, indexRegistry, quesma_v2)
 	}
 }
