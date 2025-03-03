8cc03ddc1199240f31eb9848014957d449c4e235
Przemek Delewski <pdelewski@quesma.com>
2025-03-03 18:49:43 +0100

    Update build-image

--- bin/build-image.sh
+++ bin/build-image.sh
@@ -9,4 +9,4 @@ QUESMA_VERSION="development"
 
 export DOCKER_BUILDKIT=1
 
-docker build --build-arg QUESMA_BUILD_DATE="$QUESMA_BUILD_DATE" --build-arg QUESMA_VERSION="$QUESMA_VERSION" --build-arg QUESMA_BUILD_SHA="$QUESMA_BUILD_SHA" -f platform/Dockerfile -t quesma/quesma:nightly platform
+docker build --build-arg QUESMA_BUILD_DATE="$QUESMA_BUILD_DATE" --build-arg QUESMA_VERSION="$QUESMA_VERSION" --build-arg QUESMA_BUILD_SHA="$QUESMA_BUILD_SHA" -f cli/Dockerfile -t quesma/quesma:nightly cli
