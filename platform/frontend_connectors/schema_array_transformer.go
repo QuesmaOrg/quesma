4f3e08a05284336a5178df8adc9c64ac24903a53
trzysiek <trzysiek@quesma.com>
2025-03-07 15:57:01 +0100

    Works?

--- platform/frontend_connectors/schema_array_transformer.go
+++ platform/frontend_connectors/schema_array_transformer.go
@@ -93,16 +93,22 @@ func NewArrayTypeVisitor(resolver arrayTypeResolver) model.ExprVisitor {
 		column, ok := e.Left.(model.ColumnRef)
 		if ok {
 			dbType := resolver.dbColumnType(column.ColumnName)
+			logger.Info().Msgf("Column %v (%v)", column.ColumnName, dbType)
 			if strings.HasPrefix(dbType, "Array") {
-				op := strings.ToUpper(e.Op)
-				op = strings.TrimSpace(op)
+				op := strings.TrimSpace(e.Op)
+				opUpperCase := strings.ToUpper(op)
+				logger.Info().Msgf("LOL %v", op)
 				switch {
-				case (op == "ILIKE" || op == "LIKE") && dbType == "Array(String)":
+				case (opUpperCase == "ILIKE" || opUpperCase == "LIKE") && dbType == "Array(String)":
 
 					variableName := "x"
 					lambda := model.NewLambdaExpr([]string{variableName}, model.NewInfixExpr(model.NewLiteral(variableName), op, e.Right.Accept(b).(model.Expr)))
 					return model.NewFunction("arrayExists", lambda, e.Left)
 
+				case op == model.MatchOperator && dbType == "Array(String)":
+					variableName := "x"
+					lambda := model.NewLambdaExpr([]string{variableName}, model.NewInfixExpr(model.NewLiteral(variableName), "ILIKE", e.Right.Accept(b).(model.Expr)))
+					return model.NewFunction("arrayExists", lambda, e.Left)
 				case op == "=":
 					return model.NewFunction("has", e.Left, e.Right.Accept(b).(model.Expr))
 
