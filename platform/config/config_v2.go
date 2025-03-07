4c6fd536a76c611f70e9b62ea1d012a1aa8a4eef
przemyslaw <przemyslaw.hejman@quesma.com>
2025-03-07 16:52:53 +0100

    Don't allow common table partitioning

--- platform/config/config_v2.go
+++ platform/config/config_v2.go
@@ -344,6 +344,9 @@ func (c *QuesmaNewConfiguration) validatePipelines() error {
 				if queryIndexConf.PartitioningStrategy != ingestIndexConf.PartitioningStrategy {
 					return fmt.Errorf("ingest and query processors must have the same configuration of 'partitioningStrategy' for index '%s' due to current limitations", indexName)
 				}
+				if ingestIndexConf.PartitioningStrategy != "" && ingestIndexConf.UseCommonTable { // we don't check for queryIndexConf.PartitioningStrategy per previous check
+					return fmt.Errorf("partitioning strategy cannot be set for index '%s' - common table partitioning is NOT supported", indexName)
+				}
 				allowedPartitioningStrategies := []string{"", "hourly", "daily", "monthly", "yearly"} // can't cause import cycle, but that's coming from `platform/clickhouse/clickhouse.go`
 				if !slices.Contains(allowedPartitioningStrategies, queryIndexConf.PartitioningStrategy) {
 					return fmt.Errorf("partitioning strategy '%s' is not allowed for index '%s', only %s are supported", queryIndexConf.PartitioningStrategy, indexName, allowedPartitioningStrategies)
