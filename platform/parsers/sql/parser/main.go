b4df67785177e0a218e2aa4aed5a06a5df673d10
Piotr Grabowski <piotr@quesma.com>
2025-03-11 11:21:15 +0100

    TIMEBUCKET

--- platform/parsers/sql/parser/main.go
+++ platform/parsers/sql/parser/main.go
@@ -8,20 +8,20 @@ import (
 	lexer_core "github.com/QuesmaOrg/quesma/platform/parsers/sql/lexer/core"
 	"github.com/QuesmaOrg/quesma/platform/parsers/sql/lexer/dialect_sqlparse"
 	"github.com/QuesmaOrg/quesma/platform/parsers/sql/parser/core"
+	"github.com/QuesmaOrg/quesma/platform/parsers/sql/parser/pipe_syntax"
 	"github.com/QuesmaOrg/quesma/platform/parsers/sql/parser/transforms"
-	"github.com/QuesmaOrg/quesma/platform/parsers/sql/parser/transpiler"
 )
 
 func main() {
-	tokens := lexer_core.Lex(`FROM tabela |> WHERE b = 9 |> WHERE c = 10`, dialect_sqlparse.SqlparseRules)
+	tokens := lexer_core.Lex("FROM kibana_sample_data_logs |> SELECT `@timestamp` |> CALL TIMEBUCKET `@timestamp` BY 3 HOUR AS timebucket |> AGGREGATE count(*) AS logs_count GROUP BY timebucket |> ORDER BY timebucket", dialect_sqlparse.SqlparseRules)
 
 	//tokens := lexer_core.Lex(`SELECT * FROM tabela WHERE b = 9 |> JOIN tabela2 ON b = d |> WHERE b = 3 |> ORDER BY b |> WHERE d = 9 |> SELECT a, b, c |> LIMIT 100`, dialect_sqlparse.SqlparseRules)
 	node := core.TokensToNode(tokens)
 
 	transforms.GroupParenthesis(node)
-	transforms.TransformPipeSyntax(node)
-
-	transpiler.Transpile(node)
+	pipe_syntax.GroupPipeSyntax(node)
+	pipe_syntax.ExpandMacros(node)
+	pipe_syntax.Transpile(node)
 
 	fmt.Println(transforms.ConcatTokenNodes(node))
 }
