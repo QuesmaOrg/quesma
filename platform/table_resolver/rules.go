83b92aad27ff8c4da2d80ba901a9feff9e7afb97
Przemek Delewski <pdelewski@quesma.com>
2025-05-09 13:23:01 +0200

    Comment out rules changes

--- platform/table_resolver/rules.go
+++ platform/table_resolver/rules.go
@@ -288,13 +288,13 @@ func (r *tableRegistryImpl) makeCommonTableResolver(cfg map[string]config.IndexC
 			}
 		}
 
-		connectors := []quesma_api.ConnectorDecision{&quesma_api.ConnectorDecisionClickhouse{
-			ClickhouseTableName: common_table.TableName,
-			ClickhouseIndexes:   []string{part},
-			IsCommonTable:       true,
-		}}
 		// TODO uncomment this in order to enable A/B testing
 		/*
+			connectors := []quesma_api.ConnectorDecision{&quesma_api.ConnectorDecisionClickhouse{
+				ClickhouseTableName: common_table.TableName,
+				ClickhouseIndexes:   []string{part},
+				IsCommonTable:       true,
+			}}
 			enableABTesting := false
 			if optCfg, ok := r.conf.DefaultQueryOptimizers[config.ElasticABOptimizerName]; ok {
 				if !optCfg.Disabled {
@@ -306,8 +306,12 @@ func (r *tableRegistryImpl) makeCommonTableResolver(cfg map[string]config.IndexC
 		if idxConfig, ok := cfg[part]; (ok && idxConfig.UseCommonTable) || (virtualTableExists) {
 			return &quesma_api.Decision{
 				//EnableABTesting: enableABTesting,
-				UseConnectors: connectors,
-				Reason:        "Common table will be used.",
+				UseConnectors: []quesma_api.ConnectorDecision{&quesma_api.ConnectorDecisionClickhouse{
+					ClickhouseTableName: common_table.TableName,
+					ClickhouseIndexes:   []string{part},
+					IsCommonTable:       true,
+				}},
+				Reason: "Common table will be used.",
 			}
 		}
 
