d30b44f87ab4a3479e32ab72e5d38d39c2ea16fa
Przemek Delewski <pdelewski@quesma.com>
2025-06-27 11:56:30 +0200

    Fixes, PR remarks

--- platform/ingest/ddllowerer.go
+++ platform/ingest/ddllowerer.go
@@ -8,6 +8,9 @@ import (
 	"github.com/QuesmaOrg/quesma/platform/types"
 )
 
+// The main purpose of Lowerer interface is to infers a schema from input JSON
+// data and then generates a backend-specific DDL (Data Definition Language) representation, such as a CREATE TABLE statement.
+// or other DDL commands that are needed to create or modify a table in the database.
 type Lowerer interface {
 	LowerToDDL(validatedJsons []types.JSON,
 		table *chLib.Table,
