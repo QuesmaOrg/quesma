6c0c356eec97e70393b4637967d9dbdfef610a51
Przemek Delewski <pdelewski@quesma.com>
2025-06-27 16:27:45 +0200

    Moving some stuff around

--- platform/ingest/hydrolixlowerer.go
+++ platform/ingest/hydrolixlowerer.go
@@ -9,16 +9,66 @@ import (
 	"github.com/QuesmaOrg/quesma/platform/schema"
 	"github.com/QuesmaOrg/quesma/platform/types"
 	"strings"
+	"sync/atomic"
 )
 
 type HydrolixLowerer struct {
 	virtualTableStorage persistence.JSONDatabase
+	ingestCounter       int64
 }
 
 func NewHydrolixLowerer(virtualTableStorage persistence.JSONDatabase) *HydrolixLowerer {
 	return &HydrolixLowerer{
 		virtualTableStorage: virtualTableStorage,
 	}
+}
+
+func (ip *HydrolixLowerer) GenerateIngestContent(table *chLib.Table,
+	data types.JSON,
+	inValidJson types.JSON,
+	encodings map[schema.FieldEncodingKey]schema.EncodedFieldName) ([]AlterStatement, types.JSON, []NonSchemaField, error) {
+
+	if len(table.Config.Attributes) == 0 {
+		return nil, data, nil, nil
+	}
+
+	mDiff := DifferenceMap(data, table) // TODO change to DifferenceMap(m, t)
+
+	if len(mDiff) == 0 && len(inValidJson) == 0 { // no need to modify, just insert 'js'
+		return nil, data, nil, nil
+	}
+
+	// check attributes precondition
+	if len(table.Config.Attributes) <= 0 {
+		return nil, nil, nil, fmt.Errorf("no attributes config, but received non-schema fields: %s", mDiff)
+	}
+	attrsMap, _ := BuildAttrsMap(mDiff, table.Config)
+
+	// generateNewColumns is called on original attributes map
+	// before adding invalid fields to it
+	// otherwise it would contain invalid fields e.g. with wrong types
+	// we only want to add fields that are not part of the schema e.g we don't
+	// have columns for them
+	var alterStatements []AlterStatement
+	atomic.AddInt64(&ip.ingestCounter, 1)
+	//if ok, alteredAttributesIndexes := ip.shouldAlterColumns(table, attrsMap); ok {
+	//	alterStatements = ip.generateNewColumns(attrsMap, table, alteredAttributesIndexes, encodings)
+	//}
+	// If there are some invalid fields, we need to add them to the attributes map
+	// to not lose them and be able to store them later by
+	// generating correct update query
+	// addInvalidJsonFieldsToAttributes returns a new map with invalid fields added
+	// this map is then used to generate non-schema fields string
+	attrsMapWithInvalidFields := addInvalidJsonFieldsToAttributes(attrsMap, inValidJson)
+	nonSchemaFields, err := generateNonSchemaFields(attrsMapWithInvalidFields)
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	onlySchemaFields := RemoveNonSchemaFields(data, table)
+
+	return alterStatements, onlySchemaFields, nonSchemaFields, nil
 }
 
 func (l *HydrolixLowerer) LowerToDDL(
@@ -67,6 +117,17 @@ func (l *HydrolixLowerer) LowerToDDL(
 		partitioningStrategy, defaultStrategy,
 		partitioningField, defaultField,
 		partitioningGranularity, defaultGranularity)
+
+	for i, preprocessedJson := range validatedJsons {
+		alter, onlySchemaFields, nonSchemaFields, err := l.GenerateIngestContent(table, preprocessedJson,
+			invalidJsons[i], encodings)
+		_ = alter
+		_ = onlySchemaFields
+		_ = nonSchemaFields
+		if err != nil {
+			return nil, fmt.Errorf("error BuildInsertJson, tablename: '%s' : %v", table.Name, err)
+		}
+	}
 
 	result := fmt.Sprintf(`{
   "schema": {
