654e3366072c96385bd3918017e0ee48cce92497
DongLiang-0 <1747644936@qq.com>
2025-07-23 15:41:43 +0800

    Adapt to string type time data

--- platform/model/bucket_aggregations/date_histogram.go
+++ platform/model/bucket_aggregations/date_histogram.go
@@ -222,7 +222,21 @@ func (query *DateHistogram) generateSQLForCalendarInterval() model.Expr {
 }
 
 func (query *DateHistogram) getKey(row model.QueryResultRow) int64 {
-	return row.Cols[len(row.Cols)-2].Value.(int64)
+	value := row.Cols[len(row.Cols)-2].Value
+	switch v := value.(type) {
+	case int64:
+		return v
+	case string:
+		val, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			logger.Error().Msgf("string conver to int64 failed %T", v, err)
+			return 0
+		}
+		return val
+	default:
+		logger.Error().Msgf("unsupported key type: %T", v)
+		return 0
+	}
 }
 
 func (query *DateHistogram) Interval() (interval time.Duration, ok bool) {
@@ -416,7 +430,21 @@ func (qt *DateHistogramRowsTransformer) Transform(ctx context.Context, rowsFromD
 }
 
 func (qt *DateHistogramRowsTransformer) getKey(row model.QueryResultRow) int64 {
-	return row.Cols[len(row.Cols)-2].Value.(int64)
+	value := row.Cols[len(row.Cols)-2].Value
+	switch v := value.(type) {
+	case int64:
+		return v
+	case string:
+		val, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			logger.Error().Msgf("string conver to int64 failed %T", v, err)
+			return 0
+		}
+		return val
+	default:
+		logger.Error().Msgf("unsupported key type: %T", v)
+		return 0
+	}
 }
 
 func (qt *DateHistogramRowsTransformer) nextKey(key int64) int64 {
