fae30bd86605bd5a7ba5d316da252ae8ad2c4e34
Radim Hrazdil <radim.hrazdil@whalebone.io>
2025-10-17 16:23:26 +0200

    query_result: assign result of type assertion to variable
    Addressing the static check linter.
    
    Signey-off-by: Radim Hrazdil <radim.hrazdil@whalebone.io>

--- platform/model/query_result.go
+++ platform/model/query_result.go
@@ -39,25 +39,25 @@ func (c *QueryResultCol) String(ctx context.Context) string {
 	if valueExtracted == nil {
 		return ""
 	}
-	switch valueExtracted.(type) {
+	switch v := valueExtracted.(type) {
 	case string:
-		processed, err := json.Marshal(valueExtracted)
+		processed, err := json.Marshal(v)
 		if err != nil {
-			logger.ErrorWithCtx(ctx).Err(err).Msgf("failed to marshal value %v", valueExtracted)
+			logger.ErrorWithCtx(ctx).Err(err).Msgf("failed to marshal value %v", v)
 		}
 		return fmt.Sprintf(`"%s": %s`, c.ColName, string(processed))
 	case time.Time:
 		// Format timestamp with consistent microsecond precision
-		formattedTime := valueExtracted.(time.Time).Format("2006-01-02 15:04:05.000000 -0700 MST")
+		formattedTime := v.Format("2006-01-02 15:04:05.000000 -0700 MST")
 		return fmt.Sprintf(`"%s": "%s"`, c.ColName, formattedTime)
 	case int, int64, float64, uint64, bool:
-		return fmt.Sprintf(`"%s": %v`, c.ColName, valueExtracted)
+		return fmt.Sprintf(`"%s": %v`, c.ColName, v)
 	default:
 		// Probably good to only use marshaller when necessary, so for arrays/maps,
 		// and try to handle simple cases without it
-		marshalled, err := json.Marshal(valueExtracted)
+		marshalled, err := json.Marshal(v)
 		if err != nil {
-			logger.ErrorWithCtx(ctx).Err(err).Msgf("failed to marshal value %v", valueExtracted)
+			logger.ErrorWithCtx(ctx).Err(err).Msgf("failed to marshal value %v", v)
 		}
 		return fmt.Sprintf(`"%s": %v`, c.ColName, string(marshalled))
 	}
